969afcf668f4468bd0ceb9d6d37ab7e6141f3b30
Makai Mann <makaim@stanford.edu>
2019-04-30 15:37:22 -0700

    Don't use union with shared_ptr

--- include/ops.h
+++ include/ops.h
@@ -36,19 +36,47 @@ namespace smt
    BVEXTRACT,
    SELECT,
    STORE,
-   VAR
+   VAR,
+   NUM_OPS
   };
 
-  union FunctionOrBuiltinOp
-  {
-    BuiltinOp builtin_op;
-    std::shared_ptr<AbsFunction> function;
-  };
+  // not advised to use a shared_ptr in a union
+  // makes sense
+  // TODO figure out if there's a better way than the struct implementation below
+  /* union FunctionOrBuiltinOp */
+  /* { */
+  /*   BuiltinOp builtin_op; */
+  /*   std::shared_ptr<AbsFunction> function; */
+  /* }; */
 
   struct Op
   {
     bool builtin;
-    FunctionOrBuiltinOp op;
+    BuiltinOp builtin_op;
+    std::shared_ptr<AbsFunction> function;
+
+  Op(BuiltinOp bop) : builtin(true), builtin_op(bop), function(nullptr) {};
+  Op(std::shared_ptr<AbsFunction> f) : builtin(false), builtin_op(NUM_OPS), function(f) {};
+  /* Op(const Op& o) : builtin(o.builtin) */
+  /* { */
+  /*   if(o.builtin) */
+  /*   { */
+  /*     builtin_op = o.builtin_op; */
+  /*   } */
+  /*   else */
+  /*   { */
+  /*     function = o.function; */
+  /*   } */
+  /* } */
+
+  /* ~Op() */
+  /* { */
+  /*   if(builtin) */
+  /*     { */
+        
+  /*     } */
+  /* } */
+
   };
 }
 
