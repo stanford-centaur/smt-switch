14f6d71d3e7bb571ecb2857cf89049abc2cb8cd7
Makai Mann <makaim@stanford.edu>
2019-06-03 12:50:19 -0700

    Add utils for assertions and logging

---
+++ tests/unit/unit-util.cpp
@@ -0,0 +1,14 @@
+#include "utils.h"
+
+int main()
+{
+  Log<0>("message!");
+  Log<1>("no message!");
+  #ifdef _DEBUG
+  Assert(true);
+  #else
+  // should compile to nothing
+  Assert(false);
+  #endif
+  return 0;
+}
