9315c5c8c8994ec66a5cffefcbbb915380ae912c
Makai Mann <makaim@stanford.edu>
2019-06-05 15:35:42 -0700

    Separate ops declaration and implementation

--- include/ops.h
+++ include/ops.h
@@ -142,58 +142,10 @@ constexpr std::array<std::string_view, NUM_OPS_AND_NULL> generate_primop2str()
 constexpr std::array<std::string_view, NUM_OPS_AND_NULL> primop2str =
     generate_primop2str();
 
-std::string to_string(PrimOp op) { return std::string(primop2str[op]); }
-
-bool operator==(Op o1, Op o2)
-{
-  if (o1.prim_op != o2.prim_op)
-  {
-    return false;
-  }
-  else if (o1.num_idx != o2.num_idx)
-  {
-    return false;
-  }
-  else
-  {
-    return (o1.num_idx == 0) || ((o1.num_idx == 1) && (o1.idx0 == o2.idx0))
-           || ((o1.num_idx == 2) && (o1.idx0 == o2.idx0)
-               && (o1.idx1 == o2.idx1));
-  }
-}
-
-bool operator!=(Op o1, Op o2)
-{
-  if (o1.prim_op != o2.prim_op)
-  {
-    return true;
-  }
-  else if (o1.num_idx != o2.num_idx)
-  {
-    return true;
-  }
-  else
-  {
-    return ((o1.num_idx > 1) || (o1.idx0 != o2.idx0))
-           && ((o1.num_idx != 2) || (o1.idx0 != o2.idx0)
-               || (o1.idx1 != o2.idx1));
-  }
-}
-
-std::ostream& operator<<(std::ostream& output, const Op o)
-{
- output << "(" << ::smt::to_string(o.prim_op);
- if (o.num_idx > 0)
- {
-   output << " " << o.idx0;
-  }
- if (o.num_idx == 2)
- {
-   output << " " << o.idx1;
- }
- output << ")";
- return output;
-}
+std::string to_string(PrimOp op);
+bool operator==(Op o1, Op o2);
+bool operator!=(Op o1, Op o2);
+std::ostream& operator<<(std::ostream& output, const Op o);
 
 }  // namespace smt
 
