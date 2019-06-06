ad3e768a79277b561be7bae8f5609bd437e51fa2
Makai Mann <makaim@stanford.edu>
2019-06-05 17:32:03 -0700

    Formatting

--- src/btor/boolector_fun.cpp
+++ src/btor/boolector_fun.cpp
@@ -1,56 +1,55 @@
 #include "boolector_fun.h"
 
-namespace smt
-{
+namespace smt {
 
 /* BoolectorFun implementation */
 
 BoolectorFun::~BoolectorFun()
 {
   if (!contains_op)
-    {
-      boolector_release(btor, node);
-    }
+  {
+    boolector_release(btor, node);
+  }
 }
 
 Sort BoolectorFun::get_sort() const
 {
   if (!contains_op)
-    {
-      return sort;
-    }
+  {
+    return sort;
+  }
   else
-    {
-      throw IncorrectUsageException("Can't get sort from non-UF function.");
-    }
+  {
+    throw IncorrectUsageException("Can't get sort from non-UF function.");
+  }
 }
 
 Op BoolectorFun::get_op() const
 {
   if (contains_op)
-    {
-      return op;
-    }
+  {
+    return op;
+  }
   else
-    {
-      throw IncorrectUsageException("Can't get op from UF function");
-    }
+  {
+    throw IncorrectUsageException("Can't get op from UF function");
+  }
 }
 
 std::string BoolectorFun::get_name() const
 {
   if (!contains_op)
-    {
-      const char * btor_symbol = boolector_get_symbol(btor, node);
-      std::string symbol(btor_symbol);
-      return symbol;
-    }
+  {
+    const char * btor_symbol = boolector_get_symbol(btor, node);
+    std::string symbol(btor_symbol);
+    return symbol;
+  }
   else
-    {
-      throw IncorrectUsageException("Can't get name from non-UF function.");
-    }
+  {
+    throw IncorrectUsageException("Can't get name from non-UF function.");
+  }
 }
 
 /* end BoolectorFun implementation */
 
-}
+}  // namespace smt
