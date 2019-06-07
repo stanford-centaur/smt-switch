e346f25c984f881decb434429d7a77014b97dbd2
Makai Mann <makaim@stanford.edu>
2019-06-06 17:59:16 -0700

    Add convenience script for building boolector tests

---
+++ tests/btor/README.md
@@ -0,0 +1,3 @@
+# Tests Using the Boolector Solver
+
+The boolector library is not included in this repository, please use the `build.sh` script to automatically build boolector and the tests. Note: the Makefile in this directory only links the necessary libraries for boolector built with lingeling.
---
+++ tests/btor/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ ! -d "./boolector" ]; then
+    git clone https://github.com/Boolector/boolector.git
+    cd boolector
+    ./contrib/setup-btor2tools.sh
+    ./contrib/setup-lingeling.sh
+    ./configure.sh --only-lingeling
+    cd build
+    make -j8
+    cd ../../
+fi
+
+make all
