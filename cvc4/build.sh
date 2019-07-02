38e04b3ff96fbe9874af6dc8b2c6f577374a3d23
Makai Mann <makaim@stanford.edu>
2019-07-02 14:42:30 -0700

    Add Makefile for cvc4

---
+++ cvc4/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ ! -d "./CVC4" ]; then
+    git clone https://github.com/CVC4/CVC4.git
+    cd CVC4
+    ./contrib/get-antlr-3.4
+    CXXFLAGS=-fPIC CFLAGS=-fPIC ./configure.sh --static
+    cd build
+    make -j16
+    cd ../../
+fi
