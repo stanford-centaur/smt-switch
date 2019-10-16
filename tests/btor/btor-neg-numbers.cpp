067b00ff81109223d75fba5ef208b7cf477391c5
Makai Mann <makaim@stanford.edu>
2019-10-16 11:01:22 -0700

    Add tests for negative numbers

---
+++ tests/btor/btor-neg-numbers.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+#include "assert.h"
+
+#include "boolector_factory.h"
+#include "smt.h"
+// after a full installation
+// #include "smt-switch/boolector_factory.h"
+// #include "smt-switch/smt.h"
+
+using namespace smt;
+using namespace std;
+
+int main()
+{
+  SmtSolver s = BoolectorSolverFactory::create();
+
+  Sort bvsort8 = s->make_sort(BV, 8);
+
+  Term four = s->make_value(4, bvsort8);
+  Term neg_four = s->make_value(-4, bvsort8);
+
+  assert(neg_four == s->make_value("-4", bvsort8));
+
+  try
+  {
+    Term impossible = s->make_value("-129", bvsort8);
+    assert(false);
+  }
+  catch (IncorrectUsageException & e)
+  {
+    cout << e.what() << endl;
+  }
+
+  s->assert_formula(
+      s->make_term(Not,
+                   s->make_term(Equal,
+                                s->make_term(BVAdd, four, neg_four),
+                                s->make_value(0, bvsort8))));
+  Result r = s->check_sat();
+  assert(r.is_unsat());
+
+  return 0;
+}
---
+++ tests/cvc4/cvc4-neg-numbers.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+#include "assert.h"
+
+#include "cvc4_factory.h"
+#include "smt.h"
+// after a full installation
+// #include "smt-switch/cvc4_factory.h"
+// #include "smt-switch/smt.h"
+
+using namespace smt;
+using namespace std;
+
+int main()
+{
+  SmtSolver s = CVC4SolverFactory::create();
+
+  // BitVector cases
+  Sort bvsort8 = s->make_sort(BV, 8);
+
+  Term four = s->make_value(4, bvsort8);
+  Term neg_four = s->make_value(-4, bvsort8);
+
+  assert(neg_four == s->make_value("-4", bvsort8));
+
+  try
+  {
+    Term impossible = s->make_value("-129", bvsort8);
+    assert(false);
+  }
+  catch (IncorrectUsageException & e)
+  {
+    cout << e.what() << endl;
+  }
+
+  s->push();
+  s->assert_formula(
+      s->make_term(Not,
+                   s->make_term(Equal,
+                                s->make_term(BVAdd, four, neg_four),
+                                s->make_value(0, bvsort8))));
+  Result r = s->check_sat();
+  assert(r.is_unsat());
+  s->pop();
+
+  r = s->check_sat();
+  assert(r.is_sat());
+
+  // Integer cases
+  Sort intsort = s->make_sort(INT);
+  Term five = s->make_value(5, intsort);
+  Term neg_five = s->make_value(-5, intsort);
+
+  assert(neg_five == s->make_value("-5", intsort));
+
+  s->push();
+  s->assert_formula(
+      s->make_term(Not,
+                   s->make_term(Equal,
+                                s->make_term(Plus, five, neg_five),
+                                s->make_value("0", intsort))));
+  r = s->check_sat();
+  assert(r.is_unsat());
+  s->pop();
+
+  return 0;
+}
