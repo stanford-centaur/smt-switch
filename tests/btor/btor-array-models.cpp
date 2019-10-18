06fdc10615c4346921accd6350226349579c81eb
Makai Mann <makaim@stanford.edu>
2019-10-18 16:00:39 -0700

    Add debug prints

--- tests/btor/btor-array-models.cpp
+++ tests/btor/btor-array-models.cpp
@@ -14,6 +14,7 @@ using namespace std;
 
 int main()
 {
+  std::cout << "entering main" << std::endl;
   SmtSolver s = BoolectorSolverFactory::create();
   s->set_opt("produce-models", "true");
   Sort bvsort32 = s->make_sort(BV, 32);
@@ -23,14 +24,26 @@ int main()
   Term y = s->make_term("y", bvsort32);
   Term arr = s->make_term("arr", array32_32);
 
+  std::cout << "after creating symbols" << std::endl;
+
   Term constraint = s->make_term(Equal, s->make_term(Select, arr, x0), x1);
   constraint = s->make_term(
       And, constraint, s->make_term(Equal, s->make_term(Select, arr, x1), y));
   constraint = s->make_term(And, constraint, s->make_term(Distinct, x1, y));
+
+  std::cout << "after creating constraint" << std::endl;
+
   s->assert_formula(constraint);
+
+  std::cout << "after asserting" << std::endl;
+
   Result r = s->check_sat();
 
+  std::cout << "after check_sat" << std::endl;
+
   assert(r.is_sat());
+
+  std::cout << "after assertion" << std::endl;
 
   Term arr_val = s->get_value(arr);
 
