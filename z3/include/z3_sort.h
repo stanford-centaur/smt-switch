df3f4ee37a88ec0ac7771f99054d3a724d8c8f54
Lindsey Stuntz <lindseystuntz@Lindseys-MacBook-Pro.local>
2019-12-04 14:22:35 -0800

    progress on z3

--- z3/include/z3_sort.h
+++ z3/include/z3_sort.h
@@ -1,12 +1,15 @@
 #include "sort.h"
 
+#include "z3++.h"
+
 namespace smt {
 
 class Z3Sort : public AbsSort
 	{
  public:
-  Z3Sort()
-      {};
+  //Z3Sort()
+  //    {};
+  Z3Sort(::z3::sort s) : sort(s) {};
   virtual ~Z3Sort(){};
   std::size_t hash() const override;
   unsigned int get_width() const override;
@@ -16,6 +19,9 @@ class Z3Sort : public AbsSort
   Sort get_codomain_sort() const override;
   bool compare(const Sort s) const override;
   SortKind get_sort_kind() const override;
+
+ protected:
+     ::z3::sort sort;
 
 };
 }
