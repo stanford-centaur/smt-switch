72afad89d9df5d50c6ad7954e9ef1ac56aba7d0a
Amalee Wilson <amalee@cs.stanford.edu>
2020-01-31 12:36:38 -0800

    format after fixing sort error string

--- yices2/src/yices2_sort.cpp
+++ yices2/src/yices2_sort.cpp
@@ -134,7 +134,7 @@ SortKind Yices2Sort::get_sort_kind() const
   }
   else
   {
-    std::string msg ("Unknown Yices2 type: ");
+    std::string msg("Unknown Yices2 type: ");
     msg += yices_type_to_string(type, 120, 1, 0);
     throw NotImplementedException(msg.c_str());
   }
