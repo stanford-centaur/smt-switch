f68768b989e2fb4b0c97344342093b43f3e71263
Makai Mann <makaim@stanford.edu>
2020-02-08 00:54:32 -0800

    Trying installing python3

--- scripts/travis-setup-osx-python.sh
+++ scripts/travis-setup-osx-python.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
-    echo "In OSX"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    export PATH=/usr/local/bin:/usr/local/sbin:$PATH
+    brew install python
 else
-    echo "NOT in OSX"
+    echo "NOT in OSX -- nothing to do"
 fi
-
