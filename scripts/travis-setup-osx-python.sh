a4909d6c247f934caafe667cf47163fb197cfb92
Makai Mann <makaim@stanford.edu>
2020-02-08 01:13:54 -0800

    Upgrade python

--- scripts/travis-setup-osx-python.sh
+++ scripts/travis-setup-osx-python.sh
@@ -3,7 +3,7 @@
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     export PATH=/usr/local/bin:/usr/local/sbin:$PATH
-    brew install python
+    brew upgrade python
 else
     echo "NOT in OSX -- nothing to do"
 fi
