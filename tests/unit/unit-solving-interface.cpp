c9657b6a1f09546136ebd81fd106b6f8cc96aaea
Makai Mann <makaim@stanford.edu>
2020-02-28 18:48:33 -0800

    Add to check-sat-assuming test

--- tests/unit/unit-solving-interface.cpp
+++ tests/unit/unit-solving-interface.cpp
@@ -30,15 +30,18 @@ class UnitSolveTests : public ::testing::Test,
 
 TEST_P(UnitSolveTests, CheckSatAssuming)
 {
-  Term b = s->make_symbol("b", boolsort);
+  Term b1 = s->make_symbol("b1", boolsort);
+  Term b2 = s->make_symbol("b2", boolsort);
   Term x = s->make_symbol("x", bvsort);
 
+  Term nb2 = s->make_term(Not, b2);
   Term xeq0 = s->make_term(Equal, x, s->make_term(0, bvsort));
-  s->assert_formula(s->make_term(Implies, b, xeq0));
+  Term nxeq0 = s->make_term(Not, xeq0);
+  s->assert_formula(s->make_term(Implies, b1, xeq0));
+  s->assert_formula(s->make_term(Implies, nb2, nxeq0));
 
-  s->assert_formula(s->make_term(Not, xeq0));
   ASSERT_THROW(s->check_sat_assuming(TermVec{ xeq0 }), IncorrectUsageException);
-  Result r = s->check_sat_assuming(TermVec{ b });
+  Result r = s->check_sat_assuming(TermVec{ b1, nb2 });
   ASSERT_TRUE(r.is_unsat());
 }
 
