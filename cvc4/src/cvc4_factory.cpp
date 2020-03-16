05aa7d13a3cc5cf3e6a7f7e8a3548cd6e30bebfd
Makai Mann <makaim@stanford.edu>
2020-03-16 14:03:42 -0700

    Fixes for solver factories

--- cvc4/src/cvc4_factory.cpp
+++ cvc4/src/cvc4_factory.cpp
@@ -8,7 +8,7 @@ namespace smt
 /* CVC4SolverFactory implementation */
 SmtSolver CVC4SolverFactory::create()
 {
-  return std::make_unique<CVC4Solver>();
+  return std::make_shared<CVC4Solver>();
 }
 /* end CVC4SolverFactory implementation */
 
