030be213477218892a8442cf622e80cf71d962be
Makai Mann <makaim@stanford.edu>
2020-03-18 09:08:22 -0700

    Revert "Remove constexpr from utils"
    This reverts commit 725ce9cc197169eead926db087b9acaa3a550b6d.

--- include/utils.h
+++ include/utils.h
@@ -5,38 +5,44 @@
 #include "assert.h"
 
 #ifdef _DEBUG
-#define _ASSERTIONS
+constexpr bool debug_mode = true;
+constexpr bool assertion_mode = true;
+#else
+constexpr bool debug_mode = false;
+constexpr bool assertion_mode = false;
 #endif
 
 #if defined(_ASSERTIONS) && !defined(_DEBUG)
-bool assertion_mode = true;
+constexpr bool assertion_mode = true;
 #endif
 
 #ifdef _LOGGING_LEVEL
-const std::size_t global_log_level = _LOGGING_LEVEL;
+constexpr std::size_t global_log_level = _LOGGING_LEVEL;
 #else
-const std::size_t global_log_level = 0;
+constexpr std::size_t global_log_level = 0;
 #endif
 
 inline void Assert(bool assertion)
 {
-#if defined(_ASSERTIONS)
-  assert(assertion);
-#endif
+  if constexpr (assertion_mode)
+  {
+    assert(assertion);
+  }
 }
 
 inline void Unreachable()
 {
-#if defined(_ASSERTIONS)
+  if constexpr (assertion_mode)
+  {
     assert(false);
-#endif
+  }
 }
 
 // logs to stdout
 template <std::size_t lvl>
 inline void Log(std::string msg)
 {
-  if (global_log_level >= lvl)
+  if constexpr (global_log_level >= lvl)
   {
     std::cout << msg << std::endl;
   }
