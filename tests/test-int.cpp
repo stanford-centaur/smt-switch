951f8516c4284b058d312b430e94d19727332562
Makai Mann <makaim@stanford.edu>
2020-03-27 13:49:42 -0700

    Add test for negative numbers

--- tests/test-int.cpp
+++ tests/test-int.cpp
@@ -36,6 +36,20 @@ TEST_P(IntTests, IntDiv)
   ASSERT_EQ(resval, two);
 }
 
+TEST_P(IntTests, NegValue)
+{
+  Term one = s->make_term(1, intsort);
+  ASSERT_TRUE(one->is_value());
+  Term neg_one = s->make_term(Negate, one);
+  ASSERT_FALSE(neg_one->is_value());
+  int num_children = 0;
+  for (auto c : neg_one)
+  {
+    num_children++;
+  }
+  ASSERT_EQ(num_children, 1);
+}
+
 INSTANTIATE_TEST_SUITE_P(ParameterizedSolverIntTests,
                          IntTests,
                          testing::ValuesIn(available_int_solver_enums()));
