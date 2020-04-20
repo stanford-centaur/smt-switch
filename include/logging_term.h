2aa20c9b4b11ced62d6009d1309c876c914396fc
Makai Mann <makaim@stanford.edu>
2020-04-20 11:31:23 -0700

    Keep track of symbols at logging term level

--- include/logging_term.h
+++ include/logging_term.h
@@ -10,6 +10,8 @@ class LoggingTerm : public AbsTerm
 {
  public:
   LoggingTerm(Term t, Sort s, Op o, TermVec c);
+  // this one is for making symbols
+  // passing a name implies it's a symbol
   LoggingTerm(Term t, Sort s, Op o, TermVec c, std::string r);
   virtual ~LoggingTerm();
 
@@ -23,12 +25,12 @@ class LoggingTerm : public AbsTerm
   Op get_op() const override;
   Sort get_sort() const override;
   std::string to_string() override;
+  bool is_symbolic_const() const override;
   TermIter begin() override;
   TermIter end() override;
 
   // dispatched to underlying term
   std::size_t hash() const override;
-  bool is_symbolic_const() const override;
   bool is_value() const override;
   uint64_t to_int() const override;
 
@@ -38,6 +40,7 @@ class LoggingTerm : public AbsTerm
   Op op;
   TermVec children;
   std::string repr;
+  bool is_symbol;
 
   // So LoggingSolver can access protected members:
   friend class LoggingSolver;
