ca8d15c6bb585850abff057c80896c12ffeeea1a
Makai Mann <makaim@stanford.edu>
2020-05-16 15:40:12 -0700

    License headers on googletest unit tests

--- tests/unit/unit-term-hashtable.cpp
+++ tests/unit/unit-term-hashtable.cpp
@@ -1,3 +1,19 @@
+/*********************                                                        */
+/*! \file unit-term-hashtable.cpp
+** \verbatim
+** Top contributors (to current version):
+**   Makai Mann
+** This file is part of the smt-switch project.
+** Copyright (c) 2020 by the authors listed in the file AUTHORS
+** in the top-level source directory) and their institutional affiliations.
+** All rights reserved.  See the file LICENSE in the top-level source
+** directory for licensing information.\endverbatim
+**
+** \brief Unit tests for term hashtable.
+**
+**
+**/
+
 #include "available_solvers.h"
 #include "gtest/gtest.h"
 #include "smt.h"
