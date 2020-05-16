be2cece6e805dc534c86e78679f7ac991bba588f
Makai Mann <makaim@stanford.edu>
2020-05-16 15:51:38 -0700

    License headers for msat tests

--- tests/msat/msat-ext-ops.cpp
+++ tests/msat/msat-ext-ops.cpp
@@ -1,3 +1,19 @@
+/*********************                                                        */
+/*! \file msat-ext-ops.cpp
+** \verbatim
+** Top contributors (to current version):
+**   Makai Mann
+** This file is part of the smt-switch project.
+** Copyright (c) 2020 by the authors listed in the file AUTHORS
+** in the top-level source directory) and their institutional affiliations.
+** All rights reserved.  See the file LICENSE in the top-level source
+** directory for licensing information.\endverbatim
+**
+** \brief
+**
+**
+**/
+
 #include <iostream>
 #include <memory>
 #include <vector>
