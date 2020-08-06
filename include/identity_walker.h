246fe468e6229f9fb7f585726f8b9be7649b9fec
Makai Mann <makaim@stanford.edu>
2020-08-06 11:25:53 -0700

    Add support for optional user-provided cache in IdentityWalker

--- include/identity_walker.h
+++ include/identity_walker.h
@@ -44,41 +44,66 @@ enum WalkerStepResult
  * If clear_cache is set, the cache is cleared between calls to visit
  * otherwise, the cache persists
  *
+ * The user can optionally pass a pointer to a cache. If that pointer
+ * is non-null, it will be used in place of the internal cache.
+ *
  * Important Note: The term arguments should belong to the solver provided
  * to the identity walker, otherwise the behavior is undefined.
  */
 class IdentityWalker
 {
 public:
-  IdentityWalker(smt::SmtSolver & solver, bool clear_cache)
-    : solver_(solver), clear_cache_(clear_cache) {};
+ IdentityWalker(smt::SmtSolver & solver,
+                bool clear_cache,
+                smt::UnorderedTermMap * ext_cache = nullptr)
+     : solver_(solver), clear_cache_(clear_cache), ext_cache_(ext_cache){};
 
-  /** Visit a term and all its subterms in a pre- and post-order traversal
-   *  @param term the term to visit
-   *  @return the term after visiting (returns the value of cache[term]
-   *     -- if it has been cached and returns term otherwise)
-   */
-  smt::Term visit(smt::Term & term);
-
-  /** Returns a reference to the underlying cache
-   *  Can be used to pre-populate the cache (Experts only)
-   */
-  smt::UnorderedTermMap & get_cache() { return cache_; }
-
- protected:
-  /** Visit a single term.
-   *  Implement this method in a derived class to change the behavior
-   *  of the walker
-   *  @param term the term to visit
-   *  @return a WalkerStepResult to tell the visit method how to proceed
+ /** Visit a term and all its subterms in a pre- and post-order traversal
+  *  @param term the term to visit
+  *  @return the term after visiting (returns the value of cache[term]
+  *     -- if it has been cached and returns term otherwise)
   */
-  virtual WalkerStepResult visit_term(smt::Term & term);
+ smt::Term visit(smt::Term & term);
 
-  smt::SmtSolver & solver_; /**< the solver to use for rebuilding terms */
-  smt::UnorderedTermMap cache_; /**< cache for updating terms */
-  bool clear_cache_; /**< if true, clears the cache between calls to visit */
-  bool preorder_; /**< true when the current term is being visited for the first time.
-                     For use in visit_term */
+protected:
+ /** Visit a single term.
+  *  Implement this method in a derived class to change the behavior
+  *  of the walker
+  *  @param term the term to visit
+  *  @return a WalkerStepResult to tell the visit method how to proceed
+  */
+ virtual WalkerStepResult visit_term(smt::Term & term);
+
+ /** Check if key is in cache
+  *  @param key
+  *  @return true iff the key is in the cache
+  */
+ bool in_cache(const Term & key) const;
+
+ /** Query the cache. Automatically uses ext_cache_ if non-null.
+  *  @param key the term to check in the cache
+  *  @param out this term is set to the cache result if there's a hit
+  *  @return true iff there is a cache hit
+  */
+ bool query_cache(const Term & key, Term out) const;
+
+ /** Populate the cache. Automatically uses ext_cache_ if non-null.
+  *  It will overwrite the existing mapping if the key is already in the cache
+  *  @param key the key term
+  *  @param val the value term
+  */
+ void populate_cache(const Term & key, const Term & val);
+
+ smt::SmtSolver & solver_; /**< the solver to use for rebuilding terms */
+ bool clear_cache_; /**< if true, clears the cache between calls to visit */
+ bool preorder_; /**< true when the current term is being visited for the first
+                    time. For use in visit_term */
+
+private:
+ // derived classes should interact with cache through the methods above only
+ smt::UnorderedTermMap cache_;       /**< cache for updating terms */
+ smt::UnorderedTermMap * ext_cache_; /**< external (user-provided) cache. If
+                                        non-null, used instead of cache_ */
 };
 
 }
