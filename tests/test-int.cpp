349e1c7e97d97b46cecb726ca2f6572f1d296683
Makai Mann <makaim@stanford.edu>
2020-08-28 08:37:17 -0700

    Support and test get_op for Int_To_BV and BV_To_Nat

--- tests/test-int.cpp
+++ tests/test-int.cpp
@@ -52,6 +52,34 @@ TEST_P(IntTests, IntDiv)
   ASSERT_EQ(resval, two);
 }
 
+TEST_P(IntTests, Bv2Int)
+{
+  Sort bvsort = s->make_sort(BV, 8);
+  Term bvx = s->make_symbol("bvx", bvsort);
+
+  Term intx;
+  try
+  {
+    intx = s->make_term(BV_To_Nat, bvx);
+  }
+  catch (SmtException & e)
+  {
+    std::cout << "Got exception when trying to apply BV_To_Nat: " << e.what()
+              << std::endl;
+    // it's fine if this op is not supported, just end the test
+    return;
+  }
+
+  ASSERT_TRUE(intx);
+  EXPECT_EQ(intx->get_sort(), intsort);
+  EXPECT_EQ(intx->get_op(), BV_To_Nat);
+
+  Term inty = s->make_symbol("inty", intsort);
+  Term bvy = s->make_term(Op(Int_To_BV, 8), inty);
+  EXPECT_EQ(bvy->get_sort(), bvsort);
+  EXPECT_EQ(bvy->get_op(), Op(Int_To_BV, 8));
+}
+
 INSTANTIATE_TEST_SUITE_P(
     ParameterizedSolverIntTests,
     IntTests,
