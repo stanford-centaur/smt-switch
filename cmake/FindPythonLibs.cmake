d6381fd69f101da834af04687123a76f07ed37a4
Leonard (Lenny) Truong <leonardtruong@gmail.com>
2020-10-05 16:27:13 -0700

    Update FindPythonLibs.cmake

--- cmake/FindPythonLibs.cmake
+++ cmake/FindPythonLibs.cmake
@@ -277,6 +277,8 @@ function(python_extension_module _target)
         if(Py_ENABLE_SHARED)
           target_link_libraries_with_dynamic_lookup(${_target} ${PYTHON_LIBRARIES})
         else()
+          # can't link against libpython, 
+          # see https://github.com/upscale-project/pono/issues/92 for context
           set_target_properties(${_target} PROPERTIES LINK_FLAGS "-undefined dynamic_lookup")
         endif()
     endif()
