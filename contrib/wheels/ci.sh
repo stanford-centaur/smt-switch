9e86bc202dd0ac4d60813c1b4ac2ff32faca67ae
Makai Mann <makaim@stanford.edu>
2020-11-03 18:24:07 -0800

    Install static version of gmp

--- contrib/wheels/ci.sh
+++ contrib/wheels/ci.sh
@@ -12,7 +12,7 @@ SWITCH_DIR=$DIR/../../
 
 cd $SWITCH_DIR
 mkdir -p build
-yum install -y gmp-devel libedit-devel
+yum install -y gmp-static libedit-devel
 pip install toml setuptools pexpect Cython==0.29
 python contrib/wheels/build_wheel.py bdist_wheel
 auditwheel show dist/*
