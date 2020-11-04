d6e291109b9cf705f18665efc521bf2951e6eab9
Makai Mann <makaim@stanford.edu>
2020-11-04 15:16:24 -0800

    Add Bitwuzla backend build infrastructure

--- configure.sh
+++ configure.sh
@@ -11,20 +11,22 @@ Usage: $0 [<option> ...]
 Configures the CMAKE build environment.
 
 -h, --help              display this message and exit
---prefix=STR            install directory       (default: /usr/local/)
---btor                  build boolector         (default: off)
---yices2                build yices2            (default: off)
---cvc4                  build cvc4              (default: off)
---msat                  build MathSAT           (default: off)
---btor-home=STR         custom BTOR location    (default: deps/boolector)
---yices2-home=STR       custom YICES2 location  (default: deps/yices2)
---cvc4-home=STR         custom CVC4 location    (default: deps/CVC4)
---msat-home=STR         custom MathSAT location (default: deps/mathsat)
---build-dir=STR         custom build directory  (default: build)
---debug                 build debug with debug symbols (default: off)
---static                create static libaries (default: off)
---python                compile with python bindings (default: off)
---py2                   use python2 interpreter (default: python3)
+--prefix=STR            install directory         (default: /usr/local/)
+--btor                  build boolector           (default: off)
+--bitwuzla              build bitwuzla            (default: off)
+--cvc4                  build cvc4                (default: off)
+--msat                  build MathSAT             (default: off)
+--yices2                build yices2              (default: off)
+--btor-home=STR         custom BTOR location      (default: deps/boolector)
+--bitwuzla-home=STR     custom Bitwuzla location  (default: deps/cbitwuzla)
+--cvc4-home=STR         custom CVC4 location      (default: deps/CVC4)
+--msat-home=STR         custom MathSAT location   (default: deps/mathsat)
+--yices2-home=STR       custom YICES2 location    (default: deps/yices2)
+--build-dir=STR         custom build directory    (default: build)
+--debug                 build with debug symbols  (default: off)
+--static                create static libaries    (default: off)
+--python                build python bindings     (default: off)
+--py2                   use python2 interpreter   (default: python3)
 EOF
   exit 0
 }
@@ -37,13 +39,15 @@ die () {
 build_dir=build
 install_prefix=default
 build_btor=default
-build_yices2=default
+build_bitwuzla=default
 build_cvc4=default
 build_msat=default
+build_yices2=default
 btor_home=default
-yices2_home=default
+bitwuzla_home=default
 cvc4_home=default
 msat_home=default
+yices2_home=default
 static=default
 python=default
 py2=default
@@ -67,6 +71,9 @@ do
         --btor)
             build_btor=ON
             ;;
+        --bitwuzla)
+            build_bitwuzla=ON
+            ;;
         --yices2)
             build_yices2=ON
             ;;
@@ -84,6 +91,16 @@ do
             case $btor_home in
                 /*) ;;                                      # absolute path
                 *) btor_home=$(pwd)/$btor_home ;; # make absolute path
+            esac
+            ;;
+        --bitwuzla-home) die "missing argument to $1 (see -h)" ;;
+        --bitwuzla-home=*)
+            bitwuzla_home=${1##*=}
+            # Check if bitwuzla_home is an absolute path and if not, make it
+            # absolute.
+            case $bitwuzla_home in
+                /*) ;;                                      # absolute path
+                *) bitwuzla_home=$(pwd)/$bitwuzla_home ;; # make absolute path
             esac
             ;;
         --cvc4-home) die "missing argument to $1 (see -h)" ;;
@@ -148,6 +165,10 @@ if [ $btor_home != default -a $build_btor = default ]; then
     build_btor=ON
 fi
 
+if [ $bitwuzla_home != default -a $build_bitwuzla = default ]; then
+    build_bitwuzla=ON
+fi
+
 if [ $cvc4_home != default -a $build_cvc4 = default ]; then
     build_cvc4=ON
 fi
@@ -168,6 +189,9 @@ cmake_opts="-DCMAKE_BUILD_TYPE=$build_type"
 [ $build_btor != default ] \
     && cmake_opts="$cmake_opts -DBUILD_BTOR=$build_btor"
 
+[ $build_bitwuzla != default ] \
+    && cmake_opts="$cmake_opts -DBUILD_BITWUZLA=$build_bitwuzla"
+
 [ $build_cvc4 != default ] \
     && cmake_opts="$cmake_opts -DBUILD_CVC4=$build_cvc4"
 
@@ -179,6 +203,9 @@ cmake_opts="-DCMAKE_BUILD_TYPE=$build_type"
 
 [ $btor_home != default ] \
     && cmake_opts="$cmake_opts -DBTOR_HOME=$btor_home"
+
+[ $bitwuzla_home != default ] \
+    && cmake_opts="$cmake_opts -DBITWUZLA_HOME=$bitwuzla_home"
 
 [ $cvc4_home != default ] \
     && cmake_opts="$cmake_opts -DCVC4_HOME=$cvc4_home"
