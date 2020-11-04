ed4213266800cd7b9d5f30e48b732f552a587172
Makai Mann <makaim@stanford.edu>
2020-11-03 17:46:34 -0800

    Include cvc4 in wheels build

--- contrib/wheels/ci.sh
+++ contrib/wheels/ci.sh
@@ -7,8 +7,7 @@ SWITCH_DIR=$DIR/../../
 cd $SWITCH_DIR
 mkdir -p build
 yum install -y gmp-devel libedit-devel
-pip install toml setuptools pexpect
-pip install Cython==0.29
+pip install toml setuptools pexpect Cython==0.29
 python contrib/wheels/build_wheel.py bdist_wheel
 auditwheel show dist/*
 auditwheel repair dist/*
