4d0270f8302de050e9e85d6666e8af8e807f5e65
Makai Mann <makaim@stanford.edu>
2020-12-05 13:20:35 -0800

    Remove deleter

--- include/my_ptr.h
+++ include/my_ptr.h
@@ -7,12 +7,6 @@
 // determine if my_ptr is just an alias for shared_ptr
 //#define USE_SHARED_PTR
 
-// TODO: implement static_pointer_cast for my_ptr
-// TODO: get rid of all the special deleter code? Could always be no-op
-//       doesn't really makes sense to have deleter
-//       either it's an owner and it should reference count
-//       or it's not an owner (like now) and it should have a nop deleter
-
 namespace smt {
 
 #ifdef USE_SHARED_PTR
@@ -22,59 +16,26 @@ using my_ptr = std::shared_ptr<_Tp>;
 
 #else
 
-struct nop
-{
-  template <typename T>
-  void operator()(T const &) const noexcept
-  {
-  }
-};
-
 /// adapted 20.7.12.2 unique_ptr for single objects.
 /// to my_ptr with a nop deleter and no deleted copiers
 
-template <typename _Tp, typename _Tp_Deleter = nop>
+template <typename _Tp>
 class my_ptr
 {
-  typedef std::tuple<_Tp *, _Tp_Deleter> __tuple_type;
-  typedef __tuple_type my_ptr::*__unspecified_bool_type;
+  typedef _Tp * my_ptr::*__unspecified_bool_type;
   typedef _Tp * my_ptr::*__unspecified_pointer_type;
 
  public:
   typedef _Tp * pointer;
   typedef _Tp element_type;
-  typedef _Tp_Deleter deleter_type;
 
   // Constructors.
-  my_ptr() : _M_t(pointer(), deleter_type())
-  {
-    static_assert(!std::is_pointer<deleter_type>::value,
-                  "constructed with null function pointer deleter");
-  }
+  my_ptr() : _raw_ptr(pointer()) {}
 
-  explicit my_ptr(pointer __p) : _M_t(__p, deleter_type())
-  {
-    static_assert(!std::is_pointer<deleter_type>::value,
-                  "constructed with null function pointer deleter");
-  }
-
-  my_ptr(pointer __p,
-         typename std::conditional<std::is_reference<deleter_type>::value,
-                                   deleter_type,
-                                   const deleter_type &>::type __d)
-      : _M_t(__p, __d)
-  {
-  }
-
-  my_ptr(pointer __p, typename std::remove_reference<deleter_type>::type && __d)
-      : _M_t(__p, __d)
-  {
-    static_assert(!std::is_reference<deleter_type>::value,
-                  "rvalue deleter bound to reference");
-  }
+  explicit my_ptr(pointer __p) : _raw_ptr(__p) {}
 
   // Destructor.
-  ~my_ptr() { reset(); }
+  ~my_ptr() {}
 
   // Assignment.
 
@@ -97,30 +58,18 @@ class my_ptr
     return get();
   }
 
-  pointer get() const { return std::get<0>(_M_t); }
-
-  typename std::add_lvalue_reference<deleter_type>::type get_deleter()
-  {
-    return std::get<1>(_M_t);
-  }
-
-  typename std::add_lvalue_reference<
-      typename std::add_const<deleter_type>::type>::type
-  get_deleter() const
-  {
-    return std::get<1>(_M_t);
-  }
+  pointer get() const { return _raw_ptr; }
 
   operator __unspecified_bool_type() const
   {
-    return get() == 0 ? 0 : &my_ptr::_M_t;
+    return get() == 0 ? 0 : &my_ptr::_raw_ptr;
   }
 
   // Modifiers.
   pointer release()
   {
     pointer __p = get();
-    std::get<0>(_M_t) = 0;
+    _raw_ptr = 0;
     return __p;
   }
 
@@ -128,41 +77,36 @@ class my_ptr
   {
     if (__p != get())
     {
-      get_deleter()(get());
-      std::get<0>(_M_t) = __p;
+      _raw_ptr = __p;
     }
   }
 
   void swap(my_ptr && __u)
   {
     using std::swap;
-    swap(_M_t, __u._M_t);
+    swap(_raw_ptr, __u._raw_ptr);
   }
 
   template <typename _Tp1,
-            typename _Tp1_Deleter,
             typename = typename std::enable_if<
                 std::is_convertible<_Tp1 *, _Tp *>::value>::type>
-  my_ptr(const my_ptr<_Tp1, _Tp1_Deleter> & u)
-      : _M_t((pointer)std::get<0>(u._M_t), (_Tp_Deleter)std::get<1>(u._M_t))
+  my_ptr(const my_ptr<_Tp1> & u) : _raw_ptr((pointer)u._raw_ptr)
   {
   }
 
   template <typename _Tp1,
-            typename _Tp1_Deleter,
             typename = typename std::enable_if<
                 std::is_convertible<_Tp1 *, _Tp *>::value>::type>
-  my_ptr & operator=(my_ptr<_Tp1, _Tp1_Deleter> u)
+  my_ptr & operator=(my_ptr<_Tp1> u)
   {
-    _M_t = std::make_tuple<pointer, _Tp_Deleter>(
-        (pointer)std::get<0>(u._M_t), (_Tp_Deleter)std::get<1>(u._M_t));
+    _raw_ptr = (pointer)u._raw_ptr;
     return *this;
   }
 
  private:
-  __tuple_type _M_t;
+  pointer _raw_ptr;
 
-  template <typename _Tp1, typename _Tp1_Deleter>
+  template <typename _Tp1>
   friend class my_ptr;
 };
 
