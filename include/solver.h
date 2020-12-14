ba5f6f79eeeddfe845cb9c5c4fe97346be4a5115
makaimann <makaim@stanford.edu>
2020-12-14 13:59:24 -0800

    Move default implementation of get_sequence_interpolants to cpp file

--- include/solver.h
+++ include/solver.h
@@ -19,7 +19,6 @@
 #include <string>
 #include <vector>
 
-#include "assert.h"
 #include "exceptions.h"
 #include "result.h"
 #include "smt_defs.h"
@@ -386,65 +385,7 @@ class AbsSmtSolver
    *
    */
   virtual Result get_sequence_interpolants(const TermVec & formulae,
-                                           TermVec & out_I) const
-  {
-    // we'll give a default implementation for sequence interpolants that
-    // does a loop outside the solver
-    // most solvers don't support interpolation, so this will fail with
-    // a NotImplementedException from get_interpolant
-    // for better performance, should specialize sequence_interpolants
-    // in the backend solver implementation (if supported)
-    // this way, the proof doesn't need to be regenerated for each new
-    // interpolant the solver will likely use the same proof and just manipulate
-    // it to get each sequence interpolant
-
-    size_t formulae_size = formulae.size();
-    if (formulae_size < 2)
-    {
-      throw IncorrectUsageException(
-          "Require at least 2 input formulae for sequence interpolation.");
-    }
-
-    Term A = formulae.at(0);
-    TermVec Bvec;
-    Bvec.reserve(formulae_size - 1);
-    // add to Bvec in reverse order so we can pop_back later
-    for (int i = formulae_size - 1; i >= 1; --i)
-    {
-      Bvec.push_back(formulae[i]);
-    }
-
-    // now do a loop and create an interpolant for each partition
-    // NOTE: this is likely much less performant than asking the solver
-    //       for a sequence interpolant directly (if supported)
-    while (Bvec.size())
-    {
-      // Note: have to pass the solver (defaults to solver_)
-      Term B = make_term(true);
-      for (auto tt : Bvec)
-      {
-        B = make_term(And, B, tt);
-      }
-      Term I;
-      Result r = get_interpolant(A, B, I);
-      if (!r.is_unsat())
-      {
-        return r;
-      }
-      else
-      {
-        out_I.push_back(I);
-      }
-      // move formula to A and remove from Bvec
-      // recall they were added to Bvec in reverse order
-      A = make_term(And, A, Bvec.back());
-      Bvec.pop_back();
-    }
-
-    assert(out_I.size() == formulae.size() - 1);
-    // created all the interpolants
-    return Result(UNSAT);
-  }
+                                           TermVec & out_I) const;
 
   SolverEnum get_solver_enum() { return solver_enum; };
 
