31be4eae92cc144156d8e939ae4475806415096c
makaimann <makaim@stanford.edu>
2020-12-17 20:42:22 -0800

    Just use operator== for BoolectorSort comparison

--- btor/src/boolector_sort.cpp
+++ btor/src/boolector_sort.cpp
@@ -96,58 +96,7 @@ bool BoolectorSortBase::compare(const Sort & s) const
 {
   std::shared_ptr<BoolectorSortBase> bs =
       std::static_pointer_cast<BoolectorSortBase>(s);
-  if (sk != bs->get_sort_kind())
-  {
-    // Note: bool and bv will still be equal for boolector, because always
-    // create BV sort even if it's a bool
-    return false;
-  }
-
-  switch (sk)
-  {
-    case ARRAY:
-    {
-      return (get_indexsort() == bs->get_indexsort())
-             && (get_elemsort() == bs->get_elemsort());
-      break;
-    }
-    case BOOL:
-    case BV:
-    {
-      return get_width() == bs->get_width();
-      break;
-    }
-    case FUNCTION:
-    {
-      SortVec domain_sorts = get_domain_sorts();
-      SortVec bs_domain_sorts = bs->get_domain_sorts();
-
-      if (domain_sorts.size() != bs_domain_sorts.size())
-      {
-        return false;
-      }
-      else if (get_codomain_sort() != bs->get_codomain_sort())
-      {
-        return false;
-      }
-
-      bool res = true;
-
-      for (unsigned i = 0; i < domain_sorts.size(); ++i)
-      {
-        res &= (domain_sorts[i] == bs_domain_sorts[i]);
-      }
-
-      return res;
-      break;
-    }
-    default:
-    {
-      Unreachable();
-      break;
-    }
-  }
-  return false;
+  return (sort == bs->sort);
 }
 /* end BoolectorSolver implementation */
 
