eaea71fb05fb9b856c1f04f90716ec308fb72473
yoni206 <yoni206@users.noreply.github.com>
2020-12-26 10:06:23 -0800

    Header for Generic Sorts (#146)
    * generic sort header
    
    * add tests
    
    * remove from cmakelists
    
    * size_t

---
+++ tests/test-generic-solver.cpp
@@ -0,0 +1,42 @@
+/*********************                                                        */
+/*! \file cvc4-printinh.cpp
+** \verbatim
+** Top contributors (to current version):
+**   Yoni Zohar
+** This file is part of the smt-switch project.
+** Copyright (c) 2020 by the authors listed in the file AUTHORS
+** in the top-level source directory) and their institutional affiliations.
+** All rights reserved.  See the file LICENSE in the top-level source
+** directory for licensing information.\endverbatim
+**
+** \brief
+**
+**
+**/
+#include <fstream>
+#include <cstdio>
+#include <stdexcept>
+#include <string>
+#include <array>
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <sstream>
+#include "assert.h"
+
+// note: this file depends on the CMake build infrastructure
+// specifically defined macros
+// it cannot be compiled outside of the build
+#include "test-utils.h"
+
+#include "cvc4_factory.h"
+#include "generic_sort.h"
+#include "smt.h"
+
+using namespace smt;
+using namespace std;
+
+int main()
+{
+  return 0;
+}
