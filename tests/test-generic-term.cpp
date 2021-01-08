fc1471a8c42d1695c9fe3a73475541b8a945b956
yoni206 <yoni206@gmail.com>
2021-01-08 15:55:45 -0800

    test

--- tests/test-generic-term.cpp
+++ tests/test-generic-term.cpp
@@ -30,6 +30,7 @@
 // it cannot be compiled outside of the build
 #include "cvc4_factory.h"
 #include "generic_term.h"
+#include "generic_sort.h"
 #include "gtest/gtest.h"
 #include "smt.h"
 #include "test-utils.h"
@@ -37,4 +38,23 @@
 using namespace smt;
 using namespace std;
 
-int main() { return 0; }
+int main() { 
+  cout << "Testing an integer constant" << endl;
+  Sort int_sort = make_generic_sort(INT);
+  GenericTerm one(int_sort, Op(), {}, "1");
+  GenericTerm one_prime(int_sort, Op(), {}, "1");
+  assert(one.get_id() == one_prime.get_id());
+  assert(one.hash() == one_prime.hash());
+  assert(!one.is_symbol());
+  assert(!one.is_param());
+  assert(!one.is_symbolic_const());
+  
+  cout << "Testing an integer variable" << endl;
+  GenericTerm x(int_sort, Op(), {}, "x", true);
+  GenericTerm x_prime(int_sort, Op(), {}, "x", true);
+  assert(x.get_id() == x_prime.get_id());
+  assert(x.hash() == x_prime.hash());
+  assert(x.is_symbol());
+  assert(!x.is_param());
+  assert(x.is_symbolic_const());
+}
