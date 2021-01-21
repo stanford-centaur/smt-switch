d523e236b97293c8fb05151c6d9ed8b84ec07e16
makaimann <makaim@stanford.edu>
2021-01-20 16:48:27 -0800

    Try installing with brew

--- travis-scripts/travis-setup-osx-python.sh
+++ travis-scripts/travis-setup-osx-python.sh
@@ -4,6 +4,7 @@ if [ "$TRAVIS_OS_NAME" == "osx" ]; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     export PATH=/usr/local/bin:/usr/local/sbin:$PATH
     brew upgrade python
+    brew install libantlr3c
 else
     echo "NOT in OSX -- nothing to do"
 fi
