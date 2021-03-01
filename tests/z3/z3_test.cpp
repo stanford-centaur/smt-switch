132a5e9f83822ed18317c5acc6b064a71a59a76b
lstuntz <lstuntz@stanford.edu>
2021-03-01 12:42:59 -0800

    Add Z3 functionality (terms and sorts) (#180)
    * initial z3 addition
    
    * progress on z3
    
    * initial z3 addition
    
    * progress on z3
    
    * fixed a forgotten merge thing
    
    * Fix the static library issue
    
    * Add Z3 Sort (#4)
    
    * Z3 term initial implementation
    
    * linking error state
    
    * Lindsey done fixed it
    
    * fixing build/linker issues and starting make_sort test
    
    * wed meeting stuff
    
    * non-parameterized make_sort + basic tests
    
    * add_sort for bv and uninterpreted with no tests
    
    * more sorts + start of function sort
    
    * delete extraneous comment
    
    * cleanup CMake
    
    * minor changes resolving feedback
    
    * very broken sort union progress
    
    * pointer fun
    
    * nearly complete sort :)
    
    * start to terms
    
    * op mappings + other minor changes
    
    * partial term methods
    
    * minor progress
    
    * term array progress
    
    * term progress
    
    * fixed functions and quantifiers
    
    * small changes
    
    * fixing comments
    
    Co-authored-by: Amalee Wilson <amalee@cs.stanford.edu>
    
    * Formatting and Cleanup
    
    * formatting
    
    * testing file organization
    
    * Add support for Z3 Sort (#136)
    
    * Add assert_formula, check, push, and pop
    
    * fixed whitespace discrepancy
    
    Co-authored-by: Lindsey Stuntz <lindseystuntz@Lindseys-MacBook-Pro.local>
    Co-authored-by: Lindsey Stuntz <lindseystuntz@DNa80dd66.SUNet>
    Co-authored-by: Amalee Wilson <amalee@cs.stanford.edu>

---
+++ tests/z3/z3_test.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+
+#include "assert.h"
+#include "smt.h"
+#include "z3_factory.h"
+#include "z3_sort.h"
+
+using namespace smt;
+using namespace std;
+// using namespace z3;
+
+int main()
+{
+  SmtSolver s = Z3SolverFactory::create(false);
+
+  cout << "* * * sort testing * * *" << endl;
+  Sort boolsort1 = s->make_sort(BOOL);
+  Sort realsort1 = s->make_sort(REAL);
+  Sort intsort1 = s->make_sort(INT);
+  cout << boolsort1 << endl << realsort1 << endl << intsort1 << endl;
+
+  Sort bvsort = s->make_sort(BV, 8);
+  cout << bvsort << endl;
+
+  Sort uninterpretedsort = s->make_sort("a test", 0);
+  cout << uninterpretedsort << endl;
+
+  Sort arraysort = s->make_sort(ARRAY, intsort1, bvsort);
+  cout << arraysort << endl;
+
+  Sort functionsort = s->make_sort(
+      FUNCTION, SortVec{ boolsort1, intsort1, realsort1, boolsort1 });
+  cout << functionsort << endl;
+
+  cout << "* * * end sort testing * * *" << endl
+       << "* * * basic term testing * * *" << endl;
+
+  Term boolterm1 = s->make_term(true);
+  Term boolterm2 = s->make_term(false);
+  cout << boolterm1 << endl;
+  cout << boolterm2 << endl;
+
+  Term intterm = s->make_term(1, intsort1);
+  Term realterm = s->make_term(2, realsort1);
+  Term bvterm = s->make_term(8, bvsort);
+  cout << intterm << endl;
+  cout << realterm << endl;
+  cout << bvterm << endl;
+
+  cout << "to int:" << endl;
+  cout << intterm->to_int() << endl;
+  cout << realterm->to_int() << endl;
+  cout << bvterm->to_int() << endl;
+
+  cout << "* * * end basic term testing * * *" << endl
+       << "* * * op term testing * * *" << endl;
+
+  Term notBool = s->make_term(Not, boolterm1);
+  Term isInt = s->make_term(Is_Int, intterm);
+  cout << notBool << endl;
+  cout << isInt << endl;
+
+  Term impl = s->make_term(Implies, boolterm1, boolterm1);
+  Term andBool = s->make_term(And, boolterm1, boolterm2);
+  Term concat = s->make_term(Concat, bvterm, bvterm);
+  cout << impl << endl;
+  cout << andBool << endl;
+  cout << concat << endl;
+
+  Sort bvsort2 = s->make_sort(BV, 8);
+  Term bvterm2 = s->make_term("x", bvsort2);
+  Term aext = s->make_term(Op(Extract, 3, 0), bvterm2);
+  cout << aext << endl;
+
+  Term bv2nat = s->make_term(Op(BV_To_Nat), bvterm);
+  Term int2bv = s->make_term(Op(Int_To_BV, 8), intterm);
+  cout << bv2nat << endl;
+  cout << int2bv << endl;
+
+  Term ext2 = s->make_term(Op(Zero_Extend, 1), bvterm);
+  cout << ext2 << endl;
+
+  Sort functionsort2 = s->make_sort(FUNCTION, SortVec{ boolsort1, intsort1 });
+  Sort functionsort3 =
+      s->make_sort(FUNCTION, SortVec{ boolsort1, intsort1, boolsort1 });
+  cout << "function exploration: " << endl;
+  cout << functionsort << endl;
+  Term funfun = s->make_symbol("hellooo", functionsort);
+  Term funfun2 = s->make_symbol("hellooo2", functionsort2);
+  Term funfun3 = s->make_symbol("hellooo3", functionsort3);
+  cout << funfun << endl;
+
+  Term appfun =
+      s->make_term(Op(Apply), TermVec{ funfun, boolterm1, intterm, realterm });
+  Term appfun2 = s->make_term(Op(Apply), funfun2, boolterm1);
+  Term appfun3 = s->make_term(Op(Apply), funfun3, boolterm1, intterm);
+  cout << appfun << endl;
+  cout << appfun2 << endl;
+  cout << appfun3 << endl;
+
+  Term x = s->make_symbol("x", boolsort1);
+  Term y = s->make_symbol("y", boolsort1);
+  Term impx = s->make_term(Implies, x, y);
+  Term qterm = s->make_term(Forall, TermVec{ x, impx });
+  Term eterm = s->make_term(Exists, TermVec{ x, impx });
+  cout << qterm << endl << eterm << endl;
+
+  cout << "* * * end op term testing * * *" << endl
+       << "testing done :)" << endl;
+
+  return 0;
+}
---
+++ z3/include/z3_term.h
@@ -0,0 +1,98 @@
+/*********************                                                        */
+/*! \file z3_term.h
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Lindsey Stuntz
+ ** This file is part of the smt-switch project.
+ ** Copyright (c) 2020 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved.  See the file LICENSE in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** \brief Z3 implementation of AbsTerm
+ **
+ **
+ **/
+
+#pragma once
+
+#include <vector>
+
+#include "term.h"
+#include "utils.h"
+#include "z3++.h"
+#include "z3_sort.h"
+
+namespace smt {
+
+// forward declaration
+class Z3Solver;
+
+class Z3TermIter : public TermIterBase
+{
+ public:
+  Z3TermIter(expr t, uint32_t p) : term(t), pos(p){};
+  //	Z3TermIter(const Z3TermIter &it);
+  ~Z3TermIter(){};
+  Z3TermIter & operator=(const Z3TermIter & it);
+  void operator++() override;
+  const Term operator*() override;
+  TermIterBase * clone() const override;
+  bool operator==(const Z3TermIter & it);
+  bool operator!=(const Z3TermIter & it);
+
+ protected:
+  bool equal(const TermIterBase & other) const override;
+
+ private:
+  expr term;
+  uint32_t pos;
+};
+
+class Z3Term : public AbsTerm
+{
+ public:
+  // Non-functions
+  Z3Term(expr t, context & c) : term(t), is_function(false), z_func(c)
+  {
+    ctx = &c;
+  };
+  // Functions
+  Z3Term(func_decl zfunc, context & c)
+      : term(c), is_function(true), z_func(zfunc)
+  {
+    ctx = &c;
+  };
+  ~Z3Term(){};
+  std::size_t hash() const override;
+  std::size_t get_id() const override;
+  bool compare(const Term & absterm) const override;
+  Op get_op() const override;
+  Sort get_sort() const override;
+  bool is_symbol() const override;
+  bool is_param() const override;
+  bool is_symbolic_const() const override;
+  bool is_value() const override;
+  virtual std::string to_string() override;
+  uint64_t to_int() const override;
+  /* Iterators for traversing the children */
+  TermIter begin() override;
+  TermIter end() override;
+  std::string print_value_as(SortKind sk) override;
+
+ protected:
+  expr term;
+  func_decl z_func;
+  bool is_function;
+  context * ctx;
+
+  // a const version of to_string
+  // the main to_string can't be const so that LoggingSolver
+  // can build its string representation lazily
+  std::string const_to_string() const;
+
+  friend class Z3Solver;
+  friend class Z3TermIter;
+};
+
+}  // namespace smt
