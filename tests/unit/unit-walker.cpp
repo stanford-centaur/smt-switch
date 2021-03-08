5d1c8b6f7772ba4632a55d2934e7afe9e361a4d0
Allison <ahumanlamp@gmail.com>
2021-03-08 16:29:18 -0500

    added equivalence checking for occurrences and paths

--- tests/unit/unit-walker.cpp
+++ tests/unit/unit-walker.cpp
@@ -29,7 +29,7 @@ using namespace std;
 namespace smt_tests {
 
 class DummyTreeWalker : TreeWalker {
-  TreeWalkerStepResult visit_term(smt::Term & term, std::vector<int> & path) override{
+  TreeWalkerStepResult visit_term(smt::Term & formula, smt::Term & term, std::vector<int> & path) override{
     return TreeWalker_Continue;
   }
 };
@@ -49,6 +49,8 @@ class UnitWalkerTests : public ::testing::Test,
   SmtSolver s;
   Sort bvsort, funsort, arrsort;
 };
+
+//TODO add new class for thing
 
 TEST_P(UnitWalkerTests, Simple)
 {
@@ -141,13 +143,16 @@ string vectorToString(vector<int> v){
 void printMap(UnorderedTermPairMap const &m){
   for (auto const& pair : m){
     auto val_pair = pair.second;
-    cout << "{ " << pair.first << ": " << "(" << val_pair.first << ", " << vectorToString(val_pair.second) << ") " << " }\n";
+    cout << pair.first << ": " << "<" << val_pair.first << ", (" << vectorToString(val_pair.second) << ")>" << " }\n";
     //cout << "{ " << pair.first << ": " << pair.second << " }" << endl;
   }
 }
 
 TEST_P(UnitWalkerTests, SimpleTree)
 {
+  SolverConfiguration sc = GetParam();
+  if (sc.is_logging_solver) {
+
   Term x = s->make_symbol("x", bvsort);
   Term xp1 = s->make_term(BVAdd, x, s->make_term(1, bvsort));
 
@@ -158,16 +163,133 @@ TEST_P(UnitWalkerTests, SimpleTree)
   p1 = iw.visit(xp1);
   Term xp11 = p1.first;
   EXPECT_EQ(xp1, xp11);
-//  EXPECT_EQ(p1.second, {0});
-//  vector<int> expected;
-//  expected.push_back(0);
-//  EXPECT_EQ(expected.size(), p1.second.size());
-//  EXPECT_EQ(expected[0], p1.second[0]);
-//  printVector(p1.second);
-  //assert(false);
   // visit a second time
   printMap(UndMap);
   EXPECT_EQ(xp1, xp11);
+
+  map<Term, pair<Term, vector<int>>> expected_map;
+  pair <Term, vector<int>> n0;
+  n0.first = xp1;
+  vector<int> n0path;
+  n0.second = n0path;
+  expected_map[xp1] = n0;
+  pair <Term, vector<int>> n1;
+  n1.first = xp1;
+  vector<int> n1path = {0};
+  n1.second = n1path;
+  expected_map[x] = n1;
+  pair <Term, vector<int>> n2;
+  n2.first = xp1;
+  vector<int> n2path = {1};
+  n2.second = n2path;
+  expected_map[s->make_term(1, bvsort)] = n2;
+
+  for (auto const& x : UndMap){
+//    EXPECT_EQ(x.first, expected_map[x.first]);
+    EXPECT_EQ(x.second.first, expected_map[x.first].first); //testing equivalence of first element in pair: the topmost node
+//    EXPECT_EQ(x.second.second, expected_map[x.first].second); //testing equivalence of 2nd element in pair: the path
+    ASSERT_EQ(x.second.second.size(), expected_map[x.first].second.size());
+    for (int i = 0; i < x.second.second.size(); i++){
+//      cout << "size: " << x.second.second.size() << endl;
+ //     cout <<  x.second.second[i]<< endl;
+   //   cout <<  expected_map[x.first].second[i]<< endl;
+      EXPECT_EQ(x.second.second[i], expected_map[x.first].second[i]);
+    }
+    }
+  }
+}
+
+TEST_P(UnitWalkerTests, PathDecomp)
+{
+  SolverConfiguration sc = GetParam();
+  if (sc.is_logging_solver) {
+  Term x = s->make_symbol("x", bvsort);
+  Term y = s->make_symbol("y", bvsort);
+  Term xp1 = s->make_term(BVAdd, x, s->make_term(1, bvsort));
+  Term xp1py = s->make_term(BVAdd, xp1, y); //diff than if had done ADD(x, 1, y) ?
+  Term yexp1py = s->make_term(Equal, y, xp1py);
+
+//  map<Term, vector<int>> expected_map;
+//  //y = x+1+y
+//  pair <Term, vector<int>> n0;
+//  n0.first = yexp1py;
+//  vector<int> n0path;
+//  n0.second = n0path;
+//  expected_map[yexp1py] = n0;
+//  //y: {0}, gets overwritten
+//  //x+1+y: {1}
+//  pair <Term, vector<int>> n1;
+//  n1.first = xp1py;
+//  vector<int> n1path = {1};
+//  n1.second = n1path;
+//  expected_map[xp1py] = n1;
+//  //x+1: {1,0}
+//  pair <Term, vector<int>> n2;
+//  n2.first = xp1;
+//  vector<int> n2path = {1, 0};
+//  n2.second = n2path;
+//  expected_map[xp1] = n2;
+//  //y: {1,1}
+//  pair <Term, vector<int>> n3;
+//  n3.first = y;
+//  vector<int> n3path = {1, 1};
+//  n3.second = n3path;
+//  expected_map[y] = n3;
+//  //x: {1,0,0}
+//  pair <Term, vector<int>> n4;
+//  n4.first = x;
+//  vector<int> n4path = {1, 0, 0};
+//  n4.second = n4path;
+//  expected_map[x] = n4;
+//  //1: {1,0,1}
+//  pair <Term, vector<int>> n5;
+//  n5.first = s->make_term(1, bvsort); //should be type term...
+//  vector<int> n5path = {1, 0, 1};
+//  n5.second = n5path;
+//  expected_map[s->make_term(1, bvsort)] = n5; //does this need to be turned into a term somehow?
+
+  UnorderedTermPairMap UndMap;
+  TreeWalker iw(s, false, &UndMap);
+
+  pair <Term, vector<int>> p1;
+  p1 = iw.visit(yexp1py);
+  Term xp11 = p1.first;
+//  EXPECT_EQ(yexp1py, xp11);
+//  cout << s->get_solver_enum() << endl;
+//  SolverConfiguration sc = GetParam();
+//  cout << sc.is_logging_solver << std::endl;
+  printMap(UndMap);
+
+  //for (auto const& x : UndMap){
+    //for (auto const& y : x.second){
+      //EXPECT_EQ(y.second, expected_map[x].second); //I don't think this is proper format...
+    //}
+  //}
+  }
+}
+
+TEST_P(UnitWalkerTests, PathTests1)
+{
+  SolverConfiguration sc = GetParam();
+  if (sc.is_logging_solver) {
+  Term x = s->make_symbol("x", bvsort);
+  Term y = s->make_symbol("y", bvsort);
+  Term xpx = s->make_term(BVAdd, x, x);
+  Term ypy = s->make_term(BVAdd, y, y);
+  Term xp2 = s->make_term(BVAdd, x, s->make_term(2, bvsort));
+  Term lmt = s->make_term(BVSub, xp2, xpx);
+  Term rhs = s->make_term(BVAdd, ypy, lmt);
+  Term fullform = s->make_term(Equal, xpx, rhs);
+
+  UnorderedTermPairMap UndMap;
+  TreeWalker iw(s, false, &UndMap);
+
+  pair <Term, vector<int>> p1;
+  p1 = iw.visit(fullform);
+//  Term = p1.first;
+  printMap(UndMap);
+
+  }
 }
 
 INSTANTIATE_TEST_SUITE_P(ParametrizedUnitWalker,
