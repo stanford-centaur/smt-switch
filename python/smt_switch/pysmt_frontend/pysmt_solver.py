27857defa356b5178935e5e75b5958145eafef1c
Caleb Donovick <cdonovick@users.noreply.github.com>
2021-03-10 10:50:13 -0800

    Support back (#204)

--- python/smt_switch/pysmt_frontend/pysmt_solver.py
+++ python/smt_switch/pysmt_frontend/pysmt_solver.py
@@ -4,12 +4,10 @@ import functools as ft
 import gc
 import itertools as it
 import operator
-import sys
 
 import smt_switch as ss
 
 
-from pysmt.constants import to_python_integer
 from pysmt.exceptions import (UndefinedLogicError,
         SolverReturnedUnknownResultError, ConvertExpressionError, PysmtValueError)
 from pysmt.solvers.solver import (IncrementalTrackingSolver, UnsatCoreSolver,
@@ -19,7 +17,7 @@ from pysmt.solvers.eager import EagerModel
 from pysmt.walkers import DagWalker
 from pysmt.decorators import clear_pending_pop, catch_conversion_error
 from pysmt.logics import get_logic, SMTLIB2_LOGICS
-
+from pysmt import typing as pysmt_types
 
 
 SWITCH_SOLVERS = {}
@@ -36,6 +34,7 @@ class SwitchOptions(SolverOptions):
                 solver.solver.set_opt(k, v)
             except:
                 raise PysmtValueError(f"Error setting the option '{k}={v}'")
+
 
 def _parse_real(s):
     def _parse(it):
@@ -75,7 +74,6 @@ def _parse_real(s):
     return _parse(iter(repr(s)))
 
 
-
 class _SwitchSolver(IncrementalTrackingSolver,
                    SmtLibBasicSolver,
                    SmtLibIgnoreMixin):
@@ -89,9 +87,8 @@ class _SwitchSolver(IncrementalTrackingSolver,
 
         self.solver = self._create_solver()
         self.options(self)
-        self.converter = SwitchConverter(environment,  self.solver)
         self.mgr = environment.formula_manager
-
+        self.converter = SwitchConverter(environment,  self.solver, self.mgr)
 
     def get_model(self):
         assignment = {}
@@ -103,59 +100,14 @@ class _SwitchSolver(IncrementalTrackingSolver,
     def get_value(self, item):
         self._assert_no_function_type(item)
         sort = item.get_type()
-        item = self.converter.convert(item)
-        val = self.solver.get_value(item)
-        return self._get_value(val, sort)
-
-    def _get_value(self, val, sort):
-        if sort.is_array_type():
-            args = self._get_array_value(val, sort)
-            return self.mgr.Array(*args)
-        elif sort.is_bool_type():
-            return self.mgr.Bool(bool(val))
-        elif sort.is_bv_type():
-            return self.mgr.BV(int(val), sort.width)
-        elif sort.is_function_type():
-            raise NotImplementedError()
-        elif sort.is_int_type():
-            return self.mgr.Int(int(val))
-        elif sort.is_real_type():
-            r = _parse_real(val)
-            return self.mgr.Real(r)
-        else:
-            raise ConvertExpressionError(f'Unsupported sort: {sort}')
-
-    def _get_array_value(self, arr, sort):
-        assignment = {}
-        while arr.get_op():
-            arr, idx, elem = [x for x in arr]
-            idx = self._get_value(idx, sort.index_type)
-            val = self._get_value(elem, sort.elem_type)
-            assignment[idx] = val
-
-        children = [x for x in arr]
-        if not children:
-            default = self._make_0(sort.elem_type)
-        else:
-            assert len(children) == 1
-            default = self._get_value(children[0], sort.elem_type)
-
-        return sort.index_type, default, assignment
-
-    def _make_0(self, sort):
-        if sort.is_array_type():
-            return self.mgr.Array(sort.index_type, self._make_0(sort.elem_type))
-        if sort.is_bool_type():
-            return self.mgr.Bool(0)
-        elif sort.is_bv_type():
-            return self.mgr.BV(0, sort.width)
-        elif sort.is_int_type():
-            return self.mgr.Int(0)
-        elif sort.is_real_type():
-            return self.mgr.Real(0)
-        else:
-            raise TypeError(f'Unsupported sort: {sort}')
-
+        c_item = self.converter.convert(item)
+        val = self.solver.get_value(c_item)
+        # HACK because smt-switch sometimes loses sorts
+        # we can't use back
+        # should be: `r_val = self.converter.back(val)`
+        r_val = self.converter.back_walker._convert_value(val, sort)
+        assert r_val.get_type() == sort
+        return r_val
 
     @clear_pending_pop
     def _reset_assertions(self):
@@ -240,8 +192,9 @@ if 'btor' in  ss.solvers:
         @clear_pending_pop
         def _reset_assertions(self):
             self.solver = self._create_solver()
-            self.converter = SwitchConverter(self.environment,  self.solver)
+            self.converter = SwitchConverter(self.environment,  self.solver, self.mgr)
             self.options(self)
+
 
     SWITCH_SOLVERS['btor'] = SwitchBtor
 
@@ -282,12 +235,6 @@ if 'cvc4' in ss.solvers:
         LOGICS = _build_logics(logics_params)
         _create_solver = ft.partial(ss.create_cvc4_solver, False)
 
-        @clear_pending_pop
-        def _reset_assertions(self):
-            self.solver = self._create_solver()
-            self.converter = SwitchConverter(self.environment,  self.solver)
-            self.options(self)
-
         def _exit(self):
             super()._exit()
             # ensure prompt collection of the solver object
@@ -295,6 +242,7 @@ if 'cvc4' in ss.solvers:
             gc.collect()
 
     SWITCH_SOLVERS['cvc4'] = SwitchCVC4
+
 
 def check_args(cmp, n):
     def wrapper(f):
@@ -306,6 +254,7 @@ def check_args(cmp, n):
         return walk_op
     return wrapper
 
+
 def make_walk_nary(n, primop):
     @check_args(operator.eq, n)
     def walk_op(self, formula, args, **kwargs):
@@ -313,8 +262,10 @@ def make_walk_nary(n, primop):
         return res
     return walk_op
 
+
 make_walk_unary = ft.partial(make_walk_nary, 1)
 make_walk_binary = ft.partial(make_walk_nary, 2)
+
 
 def make_walk_variadic(n, primop):
     @check_args(operator.ge, n)
@@ -326,7 +277,7 @@ def make_walk_variadic(n, primop):
 
 
 class SwitchConverter(Converter, DagWalker):
-    def __init__(self, environment, solver):
+    def __init__(self, environment, solver, mgr):
         DagWalker.__init__(self, environment)
         self.solver = solver
         self.make_term = solver.make_term
@@ -336,13 +287,14 @@ class SwitchConverter(Converter, DagWalker):
         self.declared_vars = vs = {}
         self.declared_syms = ChainMap(vs, fs)
         self.declared_sorts = {}
+        self.back_walker = BackVisitor(mgr)
 
     @catch_conversion_error
     def convert(self, formula):
         return self.walk(formula)
 
     def back(self, expr):
-        raise NotImplementedError
+        return self.back_walker.walk_dag(expr)
 
     def _convert_sort(self, sort):
         try:
@@ -516,3 +468,205 @@ class SwitchConverter(Converter, DagWalker):
     walk_array_store = make_walk_nary(3, ss.primops.Store)
 
 
+_INDEXED_OPERATORS = frozenset((
+    ss.primops.Extract,
+    ss.primops.Repeat,
+    ss.primops.Rotate_Left,
+    ss.primops.Rotate_Right,
+    ss.primops.Sign_Extend,
+    ss.primops.Zero_Extend,
+))
+
+
+class BackVisitor(ss.TermDagVisitor):
+    def __init__(self, mgr):
+        self.mgr = mgr
+        self.convertion_table = {
+            ss.primops.Abs: self._convert_abs,
+            ss.primops.And: mgr.And,
+            ss.primops.Apply: self._convert_apply,
+            ss.primops.BVAdd: mgr.BVAdd,
+            ss.primops.BVAnd: mgr.BVAnd,
+            ss.primops.BVAshr: mgr.BVAShr,
+            ss.primops.BVComp: mgr.BVComp,
+            ss.primops.BVLshr: mgr.BVLShr,
+            ss.primops.BVMul: mgr.BVMul,
+            ss.primops.BVNand: mgr.BVNand,
+            ss.primops.BVNeg: mgr.BVNeg,
+            ss.primops.BVNor: mgr.BVNor,
+            ss.primops.BVNot: mgr.BVNot,
+            ss.primops.BVOr: mgr.BVOr,
+            ss.primops.BVSdiv: mgr.BVSDiv,
+            ss.primops.BVSge: mgr.BVSGE,
+            ss.primops.BVSgt: mgr.BVSGT,
+            ss.primops.BVShl: mgr.BVLShl,
+            ss.primops.BVSle: mgr.BVSLE,
+            ss.primops.BVSlt: mgr.BVSLT,
+            ss.primops.BVSmod: mgr.BVSMod,
+            ss.primops.BVSrem: mgr.BVSRem,
+            ss.primops.BVSub: mgr.BVSub,
+            ss.primops.BVUdiv: mgr.BVUDiv,
+            ss.primops.BVUge: mgr.BVUGE,
+            ss.primops.BVUgt: mgr.BVUGT,
+            ss.primops.BVUle: mgr.BVULE,
+            ss.primops.BVUlt: mgr.BVULT,
+            ss.primops.BVUrem: mgr.BVURem,
+            ss.primops.BVXnor: mgr.BVXnor,
+            ss.primops.BVXor: mgr.BVXor,
+            ss.primops.BV_To_Nat: mgr.BVToNatural,
+            ss.primops.Concat: mgr.BVConcat,
+            ss.primops.Distinct: mgr.AllDifferent,
+            ss.primops.Div: mgr.Div,
+            ss.primops.Equal: mgr.EqualsOrIff,
+            #ss.primops.Exists:
+            ss.primops.Extract: self._convert_extract,
+            #ss.primops.Forall:
+            ss.primops.Ge: mgr.GE,
+            ss.primops.Gt: mgr.GT,
+            ss.primops.Implies: mgr.Implies,
+            #ss.primops.Int_To_BV: NOT SUPPORTED BY PYSMT
+            #ss.primops.Is_Int: NOT SUPPORTED BY PYSMT
+            ss.primops.Ite: mgr.Ite,
+            ss.primops.Le: mgr.LE,
+            ss.primops.Lt: mgr.LT,
+            ss.primops.Minus: mgr.Minus,
+            #ss.primops.Mod: NOT SUPPORTED BY PYSMT
+            ss.primops.Mult: mgr.Times,
+            ss.primops.Negate: self._convert_negate,
+            ss.primops.Not: mgr.Not,
+            ss.primops.Or: mgr.Or,
+            ss.primops.Plus: mgr.Plus,
+            ss.primops.Pow: mgr.Pow,
+            ss.primops.Repeat: mgr.BVRepeat,
+            ss.primops.Rotate_Left: mgr.BVRol,
+            ss.primops.Rotate_Right: mgr.BVRor,
+            ss.primops.Select: mgr.Select,
+            ss.primops.Sign_Extend: mgr.BVSExt,
+            ss.primops.Store: mgr.Store,
+            #ss.primops.To_Int: NOT SUPPORTED BY PYSMT
+            ss.primops.To_Real: mgr.ToReal,
+            ss.primops.Xor: mgr.Xor,
+            ss.primops.Zero_Extend: mgr.BVZExt,
+        }
+
+    def visit_term(self, term, new_children):
+        op = term.get_op()
+        if op:
+            indices = []
+            if op.num_idx:
+                assert op.primop in _INDEXED_OPERATORS
+                indices = [op.idx0]
+                if op.num_idx == 2:
+                    indices.append(op.idx1)
+                else:
+                    assert op.num_idx == 1
+            primop = op.primop
+            if primop not in self.convertion_table:
+                raise NotImplementedError()
+            return self.convertion_table[primop](*new_children, *indices)
+        elif new_children:
+            assert term.get_sort().get_sort_kind() is ss.sortkinds.ARRAY
+            Kt = self._convert_sort(term.get_sort().get_indexsort())
+            return self.mgr.Array(Kt, *new_children, {})
+        elif term.is_value():
+            return self._convert_value(term)
+        elif term.is_symbolic_const():
+            sort = self._convert_sort(term.get_sort())
+            return self.mgr.Symbol(str(term), sort)
+        else:
+            assert term.get_sort().get_sort_kind() is ss.sortkinds.FUNCTION
+            sort = self._convert_sort(term.get_sort())
+            return slf.mgr.Symbol(str(term), sort)
+
+    def _convert_sort(self, sort):
+        kind = sort.get_sort_kind()
+        if kind is ss.sortkinds.ARRAY:
+            Kt = self._convert_sort(sort.get_indexsort())
+            Vt = self._convert_sort(sort.get_elemsort())
+            return pysmt_types.ArrayType(Kt, Vt)
+        elif kind is ss.sortkinds.BOOL:
+            return pysmt_types.BOOL
+        elif kind is ss.sortkinds.BV:
+            return pysmt_types.BVType(sort.get_width())
+        elif kind is ss.sortkinds.FUNCTION:
+            domain = [self._convert_sort(s) for s in sort.get_domain_sorts()]
+            codomain = self._convert_sort(sort.get_codomain())
+            return pysmt_types.FunctionType(codomain, domain)
+        elif kind is ss.sortkinds.INT:
+            return pysmt_types.INT
+        else:
+            assert kind is ss.sortkinds.REAL
+            return pysmt_types.REAL
+
+    def _convert_value(self, term, sort=None):
+        # because smt-switch backends cannot be trusted to maintain
+        # sorts we must allow the sort to be manually passed
+        if sort is None:
+            sort = self._convert_sort(term.get_sort())
+
+        if sort.is_array_type():
+            args = self._convert_array_value(term, sort)
+            return self.mgr.Array(*args)
+        elif sort.is_bool_type():
+            return self.mgr.Bool(bool(term))
+        elif sort.is_bv_type():
+            return self.mgr.BV(int(term), sort.width)
+        elif sort.is_function_type():
+            raise NotImplementedError()
+        elif sort.is_int_type():
+            return self.mgr.Int(int(term))
+        elif sort.is_real_type():
+            r = _parse_real(term)
+            return self.mgr.Real(r)
+        else:
+            raise ConvertExpressionError(f'Unsupported sort: {sort}')
+
+    def _convert_array_value(self, arr, sort):
+        assignment = {}
+        while arr.get_op():
+            arr, idx, elem = [x for x in arr]
+            idx = self._convert_value(idx, sort.index_type)
+            val = self._convert_value(elem, sort.elem_type)
+            assignment[idx] = val
+
+        children = [x for x in arr]
+        if not children:
+            default = self._make_0(sort.elem_type)
+        else:
+            assert len(children) == 1
+            default = self._convert_value(children[0], sort.elem_type)
+        return sort.index_type, default, assignment
+
+    def _make_0(self, sort):
+        if sort.is_array_type():
+            return self.mgr.Array(sort.index_type, self._make_0(sort.elem_type))
+        if sort.is_bool_type():
+            return self.mgr.Bool(0)
+        elif sort.is_bv_type():
+            return self.mgr.BV(0, sort.width)
+        elif sort.is_int_type():
+            return self.mgr.Int(0)
+        elif sort.is_real_type():
+            return self.mgr.Real(0)
+        else:
+            raise TypeError(f'Unsupported sort: {sort}')
+
+    def _convert_abs(self, child):
+        assert child.get_type().is_int_type()
+        z = self.mgr.Int(0)
+        return self.mgr.Ite(self.mgr.GE(child, z), child, self.mgr.Minus(z, child))
+
+    def _convert_apply(self, name, *args):
+        return self.mgr.Function(name, args)
+
+    def _convert_negate(self, child):
+        T = child.get_type()
+        assert T.is_int_type() or T.is_real_type()
+        if T.is_int_type():
+            z = self.mgr.Int(0)
+        else:
+            z = self.mgr.Real(0)
+        return self.mgr.Minus(z, child)
+
+    def _convert_extract(self, child, end, start):
+        return self.mgr.BVExtract(child, start, end)
