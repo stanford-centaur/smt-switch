75b944d6d5fd21ced1cf27619af1a97a3f69e294
Allison <ahumanlamp@gmail.com>
2021-03-16 14:40:47 -0400

    formatting

--- include/tree_walker.h
+++ include/tree_walker.h
@@ -5,31 +5,34 @@
 #include "exceptions.h"
 #include "smt.h"
 
-namespace smt
-{
-  /* vector of pairs holding terms and ints that gets used within visit in the vector to_visit
-   * as a means of storing what terms we need to visit along with each term's child number relative
-   * to its parent node or a "-1 flag" that indicates all of a node's children have already been visited.
-  */
-  using TermPairVec = std::vector<std::pair<Term, int>>;
-  /* used to map from terms to their occurrences, represented as a pair of the formula it is found in and
-   * the path indicating the terms placement in the formula it is find in.
-   * A term's path is built up as list of indices where each index is a child number telling which path to follow
-   * from the topmost node to the term's place in the formula represented as a tree.
-   * For example, in the formula x+1=2, each term and its respective path is: x+1=2: []; x+1: [0]; x: [0,0]; 1: [0,1]; 2: [1]
-   */
-  using UnorderedTermPairMap = std::unordered_map<Term, std::pair<Term, std::vector<int>>>;
+namespace smt {
+/* vector of pairs holding terms and ints that gets used within visit in the
+ * vector to_visit as a means of storing what terms we need to visit along with
+ * each term's child number relative to its parent node or a "-1 flag" that
+ * indicates all of a node's children have already been visited.
+ */
+using TermPairVec = std::vector<std::pair<Term, int>>;
+/* used to map from terms to their occurrences, represented as a pair of the
+ * formula it is found in and the path indicating the terms placement in the
+ * formula it is find in. A term's path is built up as list of indices where
+ * each index is a child number telling which path to follow from the topmost
+ * node to the term's place in the formula represented as a tree. For example,
+ * in the formula x+1=2, each term and its respective path is: x+1=2: []; x+1:
+ * [0]; x: [0,0]; 1: [0,1]; 2: [1]
+ */
+using UnorderedTermPairMap =
+    std::unordered_map<Term, std::pair<Term, std::vector<int>>>;
 
-  /** \enum
-   * Walker_Continue : rebuild the current term and continue
-   * Walker_Skip     : skip this term and all subterms
-   * Walker_Abort    : abort visiting
-   */
+/** \enum
+ * Walker_Continue : rebuild the current term and continue
+ * Walker_Skip     : skip this term and all subterms
+ * Walker_Abort    : abort visiting
+ */
 enum TreeWalkerStepResult
 {
- TreeWalker_Continue=0,
- TreeWalker_Skip,
- TreeWalker_Abort
+  TreeWalker_Continue = 0,
+  TreeWalker_Skip,
+  TreeWalker_Abort
 };
 
 /** \class
@@ -44,79 +47,88 @@ enum TreeWalkerStepResult
  * is non-null, it will be used in place of the internal cache.
  *
  * Important Note: The term arguments should belong to the solver provided
-*/
+ */
 
 class TreeWalker
 {
-public:
- TreeWalker(const smt::SmtSolver & solver,
-                bool clear_cache,
-                smt::UnorderedTermPairMap * ext_cache = nullptr)
-     : solver_(solver), clear_cache_(clear_cache), ext_cache_(ext_cache){};
+ public:
+  TreeWalker(const smt::SmtSolver & solver,
+             bool clear_cache,
+             smt::UnorderedTermPairMap * ext_cache = nullptr)
+      : solver_(solver), clear_cache_(clear_cache), ext_cache_(ext_cache){};
 
- /** Visit a term and all its subterms in a post-order traversal
-  *  @param term the term to visit
-  *  @return the value of cache[term]
-  *    if it has been cached and returns pair of the term itself and the empty path otherwise
-  */
- std::pair<smt::Term, std::vector<int>> visit(smt::Term & node);
+  /** Visit a term and all its subterms in a post-order traversal
+   *  @param term the term to visit
+   *  @return the value of cache[term]
+   *    if it has been cached and returns pair of the term itself and the empty
+   * path otherwise
+   */
+  std::pair<smt::Term, std::vector<int>> visit(smt::Term & node);
 
-protected:
- /** Visit a single term.
-  *  Implement this method in a derived class to change the behavior
-  *  of the walker.
-  *  By default, builds up cache from term to term's first observed
-  *   occurrence in the formula, where an occurrence is represented
-  *   by a pair giving the topmost node (formula in which term appears)
-  *   & the path giving position in the formula of the first occurrence
-  *   of the term.
-  *  The path is represented as a vector of ints that serve as coordinates
-  *   for a given occurrence by giving a list of the child numbers to take
-  *   from the topmost node in the tree representation of the formula down
-  *   to the given occurrence in the formula. For example, an empty vector
-  *   indicates the topmost node and the vector [0,1] indicates the node
-  *   that is the 1st child of the 0th child of the topmost node going down.
-  *   Concretely, for the formula x+1=2, each term and its respective path is:
-  *     x+1=2: []
-  *     x+1: [0]
-  *     x: [0,0]
-  *     1: [0,1]
-  *     2: [1]
-  *  @param formula the term taken to visit in visit, the topmost node of the visited formula
-  *  @param term the term to visit in visit_term, a particular sub-term of the formula (some node in the tree representation of the full formula being traversed in visit)
-  *  @param path the path for the particular term in formula, which we are visiting
-  *  @return a WalkerStepResult to tell the visit method how to proceed
-  */
- virtual TreeWalkerStepResult visit_term(smt::Term & formula, smt::Term & term, std::vector<int> & path);
+ protected:
+  /** Visit a single term.
+   *  Implement this method in a derived class to change the behavior
+   *  of the walker.
+   *  By default, builds up cache from term to term's first observed
+   *   occurrence in the formula, where an occurrence is represented
+   *   by a pair giving the topmost node (formula in which term appears)
+   *   & the path giving position in the formula of the first occurrence
+   *   of the term.
+   *  The path is represented as a vector of ints that serve as coordinates
+   *   for a given occurrence by giving a list of the child numbers to take
+   *   from the topmost node in the tree representation of the formula down
+   *   to the given occurrence in the formula. For example, an empty vector
+   *   indicates the topmost node and the vector [0,1] indicates the node
+   *   that is the 1st child of the 0th child of the topmost node going down.
+   *   Concretely, for the formula x+1=2, each term and its respective path is:
+   *     x+1=2: []
+   *     x+1: [0]
+   *     x: [0,0]
+   *     1: [0,1]
+   *     2: [1]
+   *  @param formula the term taken to visit in visit, the topmost node of the
+   * visited formula
+   *  @param term the term to visit in visit_term, a particular sub-term of the
+   * formula (some node in the tree representation of the full formula being
+   * traversed in visit)
+   *  @param path the path for the particular term in formula, which we are
+   * visiting
+   *  @return a WalkerStepResult to tell the visit method how to proceed
+   */
+  virtual TreeWalkerStepResult visit_term(smt::Term & formula,
+                                          smt::Term & term,
+                                          std::vector<int> & path);
 
- /** Check if key is in cache
-  *  @param key
-  *  @return true iff the key is in the cache
-  */
- bool in_cache(const Term & key) const;
+  /** Check if key is in cache
+   *  @param key
+   *  @return true iff the key is in the cache
+   */
+  bool in_cache(const Term & key) const;
 
- /** Query the cache. Automatically uses ext_cache_ if non-null.
-  *  @param key the term to check in the cache
-  *  @param out this is set to the cache result if there's a hit
-  *  @return true iff there is a cache hit
-  */
- bool query_cache(const Term & key, std::pair <Term, std::vector<int>> & out) const;
+  /** Query the cache. Automatically uses ext_cache_ if non-null.
+   *  @param key the term to check in the cache
+   *  @param out this is set to the cache result if there's a hit
+   *  @return true iff there is a cache hit
+   */
+  bool query_cache(const Term & key,
+                   std::pair<Term, std::vector<int>> & out) const;
 
- /** Populate the cache. Automatically uses ext_cache_ if non-null.
-  *  It will overwrite the existing mapping if the key is already in the cache
-  *  @param key the key term
-  *  @param val the value pair
-  */
- void save_in_cache(const Term & key, const std::pair <Term, std::vector<int>> & val);
+  /** Populate the cache. Automatically uses ext_cache_ if non-null.
+   *  It will overwrite the existing mapping if the key is already in the cache
+   *  @param key the key term
+   *  @param val the value pair
+   */
+  void save_in_cache(const Term & key,
+                     const std::pair<Term, std::vector<int>> & val);
 
- const smt::SmtSolver & solver_; /**< the solver to use for rebuilding terms */
- bool clear_cache_; /**< if true, clears the cache between calls to visit */
+  const smt::SmtSolver & solver_; /**< the solver to use for rebuilding terms */
+  bool clear_cache_; /**< if true, clears the cache between calls to visit */
 
-private:
- // derived classes should interact with cache through the methods above only
- smt::UnorderedTermPairMap cache_;       /**< cache for updating terms */
- smt::UnorderedTermPairMap * ext_cache_; /**< external (user-provided) cache. If
-                                        non-null, used instead of cache_ */
+ private:
+  // derived classes should interact with cache through the methods above only
+  smt::UnorderedTermPairMap cache_;       /**< cache for updating terms */
+  smt::UnorderedTermPairMap * ext_cache_; /**< external (user-provided) cache.
+                                         If non-null, used instead of cache_ */
 };
 
-}
+}  // namespace smt
--- src/tree_walker.cpp
+++ src/tree_walker.cpp
@@ -1,13 +1,12 @@
 #include "tree_walker.h"
-#include <iostream>
 
+#include <iostream>
 #include <string>
 
 using namespace smt;
 using namespace std;
 
-namespace smt
-{
+namespace smt {
 
 pair<Term, vector<int>> TreeWalker::visit(Term & node)
 {
@@ -27,8 +26,9 @@ pair<Term, vector<int>> TreeWalker::visit(Term & node)
   }
 
   // out is meant to store the result of querying the query cache
-  // by default, out gives topmost node's occurence with which to query cache before continuing
-  pair <Term, vector<int>> out;
+  // by default, out gives topmost node's occurence with which to query cache
+  // before continuing
+  pair<Term, vector<int>> out;
   out.first = node;
   out.second = tree_path;
   if (query_cache(node, out))
@@ -41,35 +41,43 @@ pair<Term, vector<int>> TreeWalker::visit(Term & node)
   visit_term(node, node, tree_path);
   /* to_visit is used to store terms left to visit & is a vector of pairs, where
    * the first element is the term we are saving to visit later &
-   * the second element is an int serving as either an index for child's number (giving last index in a node's path) or as a marker for when all the children of the node have been visited:
-   *  If the int is >=0, it indicates the number for which child the term is and
-   *  If the int is -1, it serves as a flag that all children of the node have been visited and to go up a level
-  * We first push back the pair (topmost node, -1) to to_visit, because when we get back to it and pop it off, we know all the children of the
-  * topmost node have been visited and the full formula has been traversed.
-  * Before entering the loop, we also push back all the topmost node's children, so that they and all their children can be processed before
-  * getting back to the topmost node marked with a -1, indicating the traversal is finished.
-  * */
+   * the second element is an int serving as either an index for child's number
+   * (giving last index in a node's path) or as a marker for when all the
+   * children of the node have been visited: If the int is >=0, it indicates the
+   * number for which child the term is and If the int is -1, it serves as a
+   * flag that all children of the node have been visited and to go up a level
+   * We first push back the pair (topmost node, -1) to to_visit, because when we
+   * get back to it and pop it off, we know all the children of the topmost node
+   * have been visited and the full formula has been traversed. Before entering
+   * the loop, we also push back all the topmost node's children, so that they
+   * and all their children can be processed before getting back to the topmost
+   * node marked with a -1, indicating the traversal is finished.
+   * */
   TermPairVec to_visit;
-  pair<Term, int> p1 (node, -1);
+  pair<Term, int> p1(node, -1);
   // push_back topmost node flagged with a -1
   to_visit.push_back(p1);
   // initialize child_no before starting the loop
   child_no = 0;
   // push_back all of topmost node's children to prepare for the loop
-  for (auto ttt : node){
+  for (auto ttt : node)
+  {
     p1.first = ttt;
     p1.second = child_no;
     to_visit.push_back(p1);
     child_no++;
   }
 
-  // current_pair the term and int/flag we are looking at in the current iteration, last entry in to_visit
+  // current_pair the term and int/flag we are looking at in the current
+  // iteration, last entry in to_visit
   pair<Term, int> current_pair;
-  // current_term the term we are visiting in the current iteration, first element of the pair popped off of to_visit
+  // current_term the term we are visiting in the current iteration, first
+  // element of the pair popped off of to_visit
   Term current_term;
-  // pairs of term and child number or "-1 flag" that get pushed back to to_visit to visit in subsequent iterations
+  // pairs of term and child number or "-1 flag" that get pushed back to
+  // to_visit to visit in subsequent iterations
   pair<Term, int> pn;
-  while(to_visit.size())
+  while (to_visit.size())
   {
     // get last pair on to_visit, which we visit in this iteration
     current_pair = to_visit.back();
@@ -78,30 +86,40 @@ pair<Term, vector<int>> TreeWalker::visit(Term & node)
     // pop off current pair, as we visit it in this iteration
     to_visit.pop_back();
 
-    if (child_no != -1){
+    if (child_no != -1)
+    {
       // child_no != -1, so neither it nor its children have been visited
-      // child number for a term gives the last index in treepath, which is a list of child numbers creating a numbered path for an occurrence
+      // child number for a term gives the last index in treepath, which is a
+      // list of child numbers creating a numbered path for an occurrence
       tree_path.push_back(child_no);
       // visit current_term
       visit_term(node, current_term, tree_path);
-      // push back new pair with the flag -1 to indicate that it has already been visited
+      // push back new pair with the flag -1 to indicate that it has already
+      // been visited
       pn.first = current_term;
       pn.second = -1;
       to_visit.push_back(pn);
-      // push back all children of current_term along with their respective child numbers onto to_visit to visit in subsequent iterations
+      // push back all children of current_term along with their respective
+      // child numbers onto to_visit to visit in subsequent iterations
       child_no = 0;
-      // push back all children of current_term we will need to visit before popping all the way back to the current, parent term with the -1 flag
-      for (auto tt : current_term){
+      // push back all children of current_term we will need to visit before
+      // popping all the way back to the current, parent term with the -1 flag
+      for (auto tt : current_term)
+      {
         pn.first = tt;
         pn.second = child_no;
         to_visit.push_back(pn);
         child_no++;
       }
     }
-    else {
-      // child_no = -1, so this term and all the terms below it in the formula have been visited
-      // pop off last index on tree_path to reflect that we are moving up a level in the formula now that all nodes below this have already been traversed
-      if (!tree_path.empty()){
+    else
+    {
+      // child_no = -1, so this term and all the terms below it in the formula
+      // have been visited pop off last index on tree_path to reflect that we
+      // are moving up a level in the formula now that all nodes below this have
+      // already been traversed
+      if (!tree_path.empty())
+      {
         tree_path.pop_back();
       }
     }
@@ -114,15 +132,21 @@ pair<Term, vector<int>> TreeWalker::visit(Term & node)
   return out;
 }
 
-TreeWalkerStepResult TreeWalker::visit_term(Term & formula, Term & term, vector<int> & path)
+TreeWalkerStepResult TreeWalker::visit_term(Term & formula,
+                                            Term & term,
+                                            vector<int> & path)
 {
-  // default implementation of visit_term builds up cache to map from a term in the formula to a pair giving the full formula in which it occurs and the path indicating its place in the formula
+  // default implementation of visit_term builds up cache to map from a term in
+  // the formula to a pair giving the full formula in which it occurs and the
+  // path indicating its place in the formula
 
-  // occurrence of the term represented by the formula in which it is found and the path indicating its placement in the formula
-  pair <Term, vector<int>> occ;
+  // occurrence of the term represented by the formula in which it is found and
+  // the path indicating its placement in the formula
+  pair<Term, vector<int>> occ;
   occ.first = formula;
   occ.second = path;
-  // save mapping from term we're visiting to the pair containing the formula it occurs in and its path indicating its place in the formula
+  // save mapping from term we're visiting to the pair containing the formula it
+  // occurs in and its path indicating its place in the formula
   save_in_cache(term, occ);
 
   return TreeWalker_Continue;
@@ -140,7 +164,8 @@ bool TreeWalker::in_cache(const Term & key) const
   }
 }
 
-bool TreeWalker::query_cache(const Term & key, pair <Term, vector<int>> & out) const
+bool TreeWalker::query_cache(const Term & key,
+                             pair<Term, vector<int>> & out) const
 {
   if (ext_cache_)
   {
@@ -163,7 +188,8 @@ bool TreeWalker::query_cache(const Term & key, pair <Term, vector<int>> & out) c
   return false;
 }
 
-void TreeWalker::save_in_cache(const Term & key, const pair <Term, vector<int>> & val)
+void TreeWalker::save_in_cache(const Term & key,
+                               const pair<Term, vector<int>> & val)
 {
   if (ext_cache_)
   {
@@ -174,4 +200,4 @@ void TreeWalker::save_in_cache(const Term & key, const pair <Term, vector<int>> 
     cache_[key] = val;
   }
 }
-}
+}  // namespace smt
