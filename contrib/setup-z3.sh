e72a6cb5875bb1f9c4248417ec37bafacaee449e
Makai Mann <makaim@stanford.edu>
2021-03-16 21:43:58 -0700

    Update comment

--- contrib/setup-z3.sh
+++ contrib/setup-z3.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# issues creating a static binary with z3
-# (related to pthread)
-# see https://github.com/Z3Prover/z3/issues/4554
-
 Z3_VERSION=6cc52e04c3ea7e2534644a285d231bdaaafd8714
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -17,6 +13,10 @@ if [ ! -d "$DEPS/z3" ]; then
     chmod -R 777 z3
     cd z3
     git checkout -f $Z3_VERSION
+    # a static binary linked to z3 often segfaults due to
+    # a pthread related issue
+    # compiling with --single-threaded helps, but isn't a real solution
+    # see https://github.com/Z3Prover/z3/issues/4554
     ./configure --staticlib --single-threaded
     cd build
     make -j$(nproc)
