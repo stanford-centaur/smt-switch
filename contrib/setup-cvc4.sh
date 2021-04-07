e95e8955b6ab3ac220bc387d31151873505f0fb0
Makai Mann <makaim@stanford.edu>
2021-04-07 19:45:18 -0400

    Change travis-scripts setup-cvc4 to download it

--- contrib/setup-cvc4.sh
+++ contrib/setup-cvc4.sh
@@ -1,35 +1,58 @@
 #!/bin/bash
 
-CVC4_VERSION=3dda54ba7e6952060766775c56969ab920430a8a
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 DEPS=$DIR/../deps
 
 mkdir -p $DEPS
 
+
+usage () {
+    cat <<EOF
+Usage: $0 [<option> ...]
+
+Downloads a precompiled CVC4 SMT Solver,
+
+-h, --help              display this message and exit
+EOF
+    exit 0
+}
+
+while [ $# -gt 0 ]
+do
+    case $1 in
+        -h|--help) usage;;
+        *) die "unexpected argument: $1";;
+    esac
+    shift
+done
+
 if [ ! -d "$DEPS/CVC4" ]; then
     cd $DEPS
-    git clone https://github.com/CVC4/CVC4.git
-    chmod -R 777 CVC4
-    cd CVC4
-    git checkout -f $CVC4_VERSION
-    ./contrib/get-antlr-3.4
-    git clone https://github.com/uiri/toml.git
-    export PYTHONPATH=$PYTHONPATH:`pwd`/toml
-    CXXFLAGS=-fPIC CFLAGS=-fPIC ./configure.sh --static --no-static-binary
-    cd build
-    make -j4
-    cd $DIR
+    if [[ "$OSTYPE" == linux* ]]; then
+        curl -o CVC4.tar.bz2 -L http://web.stanford.edu/~makaim/files/CVC4-linux-smtswitch.tar.bz2
+    elif [[ "$OSTYPE" == darwin* ]]; then
+        curl -o CVC4.tar.bz2 -L http://web.stanford.edu/~makaim/files/CVC4-mac-smtswitch.tar.bz2
+    elif [[ "$OSTYPE" == msys* ]]; then
+        echo "Pre-compiled libraries for Windows not yet available"
+    elif [[ "$OSTYPE" == cygwin* ]]; then
+        curl -o CVC4.tar.bz2 -L http://web.stanford.edu/~makaim/files/CVC4-linux-smtswitch.tar.bz2
+    else
+        echo "Unrecognized OSTYPE=$OSTYPE"
+        exit 1
+    fi
+
+    tar -xf CVC4.tar.bz2
+    rm CVC4.tar.bz2
+
 else
-    echo "$DEPS/CVC4 already exists. If you want to rebuild, please remove it manually."
+    echo "$DEPS/CVC4 already exists. If you want to re-download, please remove it manually."
+    exit 1
 fi
 
-if [ -f $DEPS/CVC4/build/src/libcvc4.a ] && [ -f $DEPS/CVC4/build/src/parser/libcvc4parser.a ]; then
+if [ -f $DEPS/CVC4/build/src/libcvc4.a ] ; then \
     echo "It appears CVC4 was setup successfully into $DEPS/CVC4."
     echo "You may now install it with make ./configure.sh --cvc4 && cd build && make"
 else
-    echo "Building CVC4 failed."
-    echo "You might be missing some dependencies."
-    echo "Please see their github page for installation instructions: https://github.com/CVC4/CVC4"
+    echo "Downloading CVC4 failed."
     exit 1
 fi
