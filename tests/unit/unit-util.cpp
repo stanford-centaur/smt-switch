985b5374e20ca4d4196766b0b1a10f83dfc9d5f6
BhaskarGupta22 <50695356+BhaskarGupta22@users.noreply.github.com>
2021-07-21 20:31:44 +0530

    testing

--- tests/unit/unit-util.cpp
+++ tests/unit/unit-util.cpp
@@ -237,9 +237,7 @@ TEST_P(UnitUtilDimacsTests, tseitin)
   Term q = s->make_symbol("q", boolsort);
   Term r = s->make_symbol("r", boolsort);
   Term t = s->make_symbol("t", boolsort);
-  if(s->get_solver_enum()==YICES2 || s->get_solver_enum()==Z3){
-    return;
-  }
+  
   //a=((p or q) and r) implies (not t)
   Term a = s->make_term(Implies, s->make_term(And, s->make_term(Or, p, q), r), s->make_term(Not, t));
   Term cnf1 = to_cnf(a, s);
