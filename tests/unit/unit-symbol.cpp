72ca933ede3e0e89d772f1d2b655ac0d2d8799b2
makaimann <makaim@stanford.edu>
2021-07-21 10:56:23 -0400

    Fix: keep track of symbol declarations in Yices2 and Z3 (#255)
    * Add test for re-declaring symbols
    
    * Throw exception in Yices2 if redeclaring a symbol
    
    * Keep track of declared symbols in Z3
    
    * Fix z3_test

---
+++ tests/unit/unit-symbol.cpp
@@ -0,0 +1,63 @@
+/*********************                                                        */
+/*! \file unit-symbol.cpp
+** \verbatim
+** Top contributors (to current version):
+**   Makai Mann
+** This file is part of the smt-switch project.
+** Copyright (c) 2020 by the authors listed in the file AUTHORS
+** in the top-level source directory) and their institutional affiliations.
+** All rights reserved.  See the file LICENSE in the top-level source
+** directory for licensing information.\endverbatim
+**
+** \brief Unit tests for symbol terms.
+**
+**
+**/
+
+#include <utility>
+#include <vector>
+
+#include "available_solvers.h"
+#include "gtest/gtest.h"
+#include "smt.h"
+
+using namespace smt;
+using namespace std;
+
+namespace smt_tests {
+
+class UnitSymbolTests : public ::testing::Test,
+                        public testing::WithParamInterface<SolverConfiguration>
+{
+ protected:
+  void SetUp() override
+  {
+    s = create_solver(GetParam());
+
+    boolsort = s->make_sort(BOOL);
+    bvsort = s->make_sort(BV, 4);
+    funsort = s->make_sort(FUNCTION, SortVec{ bvsort, bvsort });
+    arrsort = s->make_sort(ARRAY, bvsort, bvsort);
+  }
+  SmtSolver s;
+  Sort boolsort, bvsort, funsort, arrsort;
+};
+
+TEST_P(UnitSymbolTests, RedeclareException)
+{
+  Term b = s->make_symbol("b", boolsort);
+  Term x = s->make_symbol("x", bvsort);
+  Term f = s->make_symbol("f", funsort);
+  Term a = s->make_symbol("a", arrsort);
+
+  EXPECT_THROW(s->make_symbol("b", boolsort), IncorrectUsageException);
+  EXPECT_THROW(s->make_symbol("x", bvsort), IncorrectUsageException);
+  EXPECT_THROW(s->make_symbol("f", funsort), IncorrectUsageException);
+  EXPECT_THROW(s->make_symbol("a", arrsort), IncorrectUsageException);
+}
+
+INSTANTIATE_TEST_SUITE_P(ParameterizedSolverUnitSymbol,
+                         UnitSymbolTests,
+                         testing::ValuesIn(available_solver_configurations()));
+
+}  // namespace smt_tests
--- yices2/src/yices2_solver.cpp
+++ yices2/src/yices2_solver.cpp
@@ -644,6 +644,12 @@ Sort Yices2Solver::make_sort(const Sort & sort_con, const SortVec & sorts) const
 
 Term Yices2Solver::make_symbol(const std::string name, const Sort & sort)
 {
+  if (symbols.find(name) != symbols.end())
+  {
+    throw IncorrectUsageException("symbol " + name + " has already been used.");
+  }
+  symbols.insert(name);
+
   shared_ptr<Yices2Sort> ysort = static_pointer_cast<Yices2Sort>(sort);
   term_t y_term = yices_new_uninterpreted_term(ysort->type);
   yices_set_term_name(y_term, name.c_str());
