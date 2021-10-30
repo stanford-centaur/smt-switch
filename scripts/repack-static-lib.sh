72fe5c916b689371188940f13d8462aa9977c03c
makaimann <makaim@stanford.edu>
2021-10-30 16:23:07 -0400

    Update repack-static-lib.sh for Mac and use for cvc5

--- scripts/repack-static-lib.sh
+++ scripts/repack-static-lib.sh
@@ -1,24 +1,46 @@
 #!/bin/bash
 
 if [ $# -lt 2 ]; then
-   echo "usage: $0 <libname> [...libs to combine]"
-   exit 1
+    echo "usage: $0 <libname> [...libs to combine]"
+    exit 1
 fi
 
-TARGET=$1
+if [[ "$OSTYPE" == linux* || "$OSTYPE" == cygwin* ]]; then
+    # use a GNU ar MRI script on Linux-like systems
+    if ! command -v ar &> /dev/null
+    then
+        echo "ar could not be found"
+        echo "required for repacking static libraries on Linux"
+        exit
+    fi
 
-MRI_COMMAND="create $TARGET"
+    TARGET=$1
+    MRI_COMMAND="create $TARGET"
+    for lib in "${@:2}"; do
+        MRI_COMMAND="${MRI_COMMAND}\naddlib $lib"
+    done
 
-for lib in "${@:2}"; do
-    MRI_COMMAND="${MRI_COMMAND}\naddlib $lib"
-done
+    MRI_COMMAND="${MRI_COMMAND}\nsave\nend"
+    echo -e "$MRI_COMMAND" | ar -M
 
-MRI_COMMAND="${MRI_COMMAND}\nsave\nend"
+    if [ ! -f "${TARGET}" ]; then
+        echo "It appears ar failed to create ${TARGET}"
+        exit 1
+    fi
+elif [[ "$OSTYPE" == darwin* ]]; then
+    # use libtool (note: not the same as GNU libtool) on OSX
+    if ! command -v libtool &> /dev/null
+    then
+        echo "libtool could not be found"
+        echo "required for repacking static libraries on Mac"
+        exit
+    fi
 
-echo -e "$MRI_COMMAND" | ar -M
-
-if [ ! -f "${TARGET}" ]; then
-    echo "It appears ar failed to create ${TARGET}"
+    libtool -static -o $@
+elif [[ "$OSTYPE" == msys* ]]; then
+    echo "$0 does not support repacking static libs on Windows yet"
+else
+    echo "Unrecognized OSTYPE=$OSTYPE"
     exit 1
 fi
 
