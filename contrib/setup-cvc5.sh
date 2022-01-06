392e284c26e7436fada6224a4a29beb1dac876da
George Rennie <georgerennie@gmail.com>
2022-01-06 21:14:12 +0000

    Build CVC5 using $(nproc) threads
    * This works on Darwin or Linux in line with setup-bitwuzla.sh and setup-btor.sh

--- contrib/setup-cvc5.sh
+++ contrib/setup-cvc5.sh
@@ -7,6 +7,14 @@ mkdir -p $DEPS
 
 CVC5_VERSION=77d0bec48a745e3c4acd65085f9c59bdfceed6c0
 
+if [ "$(uname)" == "Darwin" ]; then
+    NUM_CORES=$(sysctl -n hw.logicalcpu)
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    NUM_CORES=$(nproc)
+else
+    NUM_CORES=1
+fi
+
 if [ ! -d "$DEPS/cvc5" ]; then
     cd $DEPS
     git clone https://github.com/cvc5/cvc5.git
@@ -15,7 +23,7 @@ if [ ! -d "$DEPS/cvc5" ]; then
     git checkout -f ${CVC5_VERSION}
     CXXFLAGS=-fPIC CFLAGS=-fPIC ./configure.sh --static --auto-download
     cd build
-    make -j4
+    make -j$NUM_CORES
     cd $DIR
 else
     echo "$DEPS/cvc5 already exists. If you want to rebuild, please remove it manually."
