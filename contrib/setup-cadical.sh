0bec54d44e378ddbfdc64654eba89b4184d0b52b
Áron Ricardo Perez-Lopez <arpl@cs.stanford.edu>
2024-07-05 14:52:30 +0200

    Install cadical headers

--- contrib/setup-cadical.sh
+++ contrib/setup-cadical.sh
@@ -10,11 +10,14 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ROOT_DIR="$(cd "$SCRIPT_DIR/.." && pwd)"
 DEPS_DIR="$ROOT_DIR/deps"
 
+# Download
 mkdir -p "$DEPS_DIR"
 cd $DEPS_DIR
 if [[ ! -d "cadical" ]]; then
     git clone https://github.com/arminbiere/cadical
 fi
+
+# Build
 cd cadical
 git checkout $CADICAL_VERSION
 if [[ -d "build" ]]; then
@@ -23,5 +26,10 @@ else
     CXXFLAGS=-fPIC ./configure
 fi
 make
+
+# Install
 mkdir -p "$DEPS_DIR/install/lib"
 install -m644 "build/libcadical.a" "$DEPS_DIR/install/lib"
+mkdir -p "$DEPS_DIR/install/include"
+install -m644 "src/ccadical.h" "$DEPS_DIR/install/include"
+install -m644 "src/cadical.hpp" "$DEPS_DIR/install/include"
