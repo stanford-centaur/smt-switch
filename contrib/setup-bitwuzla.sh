a395d1e42cd10423069e947ae65ef8e680239c54
Áron Ricardo Perez-Lopez <arpl@cs.stanford.edu>
2024-08-31 00:23:50 +0200

    Fix deps directory creation

--- contrib/setup-bitwuzla.sh
+++ contrib/setup-bitwuzla.sh
@@ -3,7 +3,7 @@ set -e
 
 BITWUZLA_VERSION=0e81e616af4d4421729884f01928b194c3536c76
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-DEPS="$( cd "$( dirname "$DIR" )/deps" && pwd )"
+DEPS="$( dirname "$DIR" )/deps"
 
 mkdir -p $DEPS
 
