ea5921903c027a31f59cce9c7f01863f9f113dce
Makai Mann <makaim@stanford.edu>
2025-02-17 18:10:56 -0500

    First pass on cibuildwheel infrastructure

---
+++ ci-scripts/cibw_build_before.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+# This script is meant to build the smt-switch C++ libraries for the PyPi upload
+# cibuildwheel (abbreviated cibw) will take care of building and packaging the Python wheel
+
+./contrib/setup-bitwuzla.sh
+./contrib/setup-cvc5.sh
+./contrib/setup-z3.sh
+
+source ./.venv/bin/activate
+./configure.sh --bitwuzla --cvc5 --z3 --python
+cd build
+make -j
