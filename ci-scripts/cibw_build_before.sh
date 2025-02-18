a76bb33f989d0e16fce6623b2c4d3e654aa1b2d8
Makai Mann <makaim@stanford.edu>
2025-02-18 17:51:49 -0500

    Temporarily reduce the number of solvers

--- ci-scripts/cibw_build_before.sh
+++ ci-scripts/cibw_build_before.sh
@@ -4,10 +4,11 @@
 # cibuildwheel (abbreviated cibw) will take care of building and packaging the Python wheel
 
 ./contrib/setup-bitwuzla.sh
-./contrib/setup-cvc5.sh
-./contrib/setup-z3.sh
+# ./contrib/setup-cvc5.sh
+# ./contrib/setup-z3.sh
 
 source ./.venv/bin/activate
-./configure.sh --bitwuzla --cvc5 --z3 --python
+# ./configure.sh --bitwuzla --cvc5 --z3 --python
+./configure.sh --bitwuzla --python
 cd build
 make -j
