9ed55090b56eb047ea39b2f9c426ddc2cd41c863
Makai Mann <makaim@stanford.edu>
2025-02-19 14:24:57 -0500

    Call realpath on python3

--- ci-scripts/cibw_build_before.sh
+++ ci-scripts/cibw_build_before.sh
@@ -6,11 +6,21 @@
 # Find the Python root directory for the current Python version
 # This is important for the manylinux infrastructure, which is in
 # a nonstandard location that CMake has trouble finding
-PYTHON_EXECUTABLE=$(which python3)
-PYTHON_ROOT=$(dirname $(dirname $(realpath ${PYTHON_EXECUTABLE})))
+PYTHON_EXECUTABLE=$(realpath $(which python3))
+PYTHON_ROOT=$(dirname $(dirname ${PYTHON_EXECUTABLE}))
 
 echo "Identified Python executable and root directory as:"
+echo "Python_EXECUTABLE: ${PYTHON_EXECUTABLE}"
 echo "Python_ROOT_DIR: ${PYTHON_ROOT}"
+
+# TODO: determine if we need this or not
+# if [ "$(uname)" = "Darwin" ]; then
+#     echo "Running on macOS - using Python executable only" &&
+#     CMAKE_PYTHON_OPTIONS="--python-executable=${PYTHON_EXECUTABLE}"
+# else
+#     echo "Running on Linux - using python root dir" &&
+#     CMAKE_PYTHON_OPTIONS="--python-root-dir=$PYTHON_ROOT"
+# fi
 
 ./contrib/setup-bitwuzla.sh
 ./contrib/setup-cvc5.sh
