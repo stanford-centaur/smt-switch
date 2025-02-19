a51bc99af675b5f127e083bb99e362bf45f23078
Makai Mann <makaim@stanford.edu>
2025-02-19 13:09:29 -0500

    Fix typo and print more info

--- ci-scripts/cibw_build_before.sh
+++ ci-scripts/cibw_build_before.sh
@@ -3,15 +3,19 @@
 # This script is meant to build the smt-switch C++ libraries for the PyPi upload
 # cibuildwheel (abbreviated cibw) will take care of building and packaging the Python wheel
 
-./contrib/setup-bitwuzla.sh
-./contrib/setup-cvc5.sh
-./contrib/setup-z3.sh
-
 # Find the Python root directory for the current Python version
 # This is important for the manylinux infrastructure, which is in
 # a nonstandard location that CMake has trouble finding
-PYTHON_EXECUABLE=$(which python3)
+PYTHON_EXECUTABLE=$(which python3)
 PYTHON_ROOT=$(dirname $(dirname $(realpath ${PYTHON_EXECUTABLE})))
+
+echo "Identified Python executable and root directory as:"
+echo "Python_EXECUTABLE: ${PYTHON_EXECUTABLE}"
+echo "Python_ROOT_DIR: ${PYTHON_ROOT}"
+
+./contrib/setup-bitwuzla.sh
+./contrib/setup-cvc5.sh
+./contrib/setup-z3.sh
 
 ./configure.sh --bitwuzla --cvc5 --z3 --python --python-executable=${PYTHON_EXECUTABLE} --python-root-dir=${PYTHON_ROOT}
 cd build
