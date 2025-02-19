8980974741b3632d64a9101fc6a8ba6e01839542
Makai Mann <makaim@stanford.edu>
2025-02-18 22:27:06 -0500

    Revert "Debug prints"
    This reverts commit a161edd437b525002897ec9a34ac92e31e1b0668.

--- ci-scripts/cibw_build_before.sh
+++ ci-scripts/cibw_build_before.sh
@@ -4,20 +4,15 @@
 # cibuildwheel (abbreviated cibw) will take care of building and packaging the Python wheel
 
 ./contrib/setup-bitwuzla.sh
-# ./contrib/setup-cvc5.sh
-# ./contrib/setup-z3.sh
+./contrib/setup-cvc5.sh
+./contrib/setup-z3.sh
 
 source ./.venv/bin/activate
-# ./configure.sh --bitwuzla --cvc5 --z3 --python --python-executable=$(which python3)
-./configure.sh --bitwuzla --python --python-executable=$(which python3)
+./configure.sh --bitwuzla --cvc5 --z3 --python --python-executable=$(which python3)
 cd build
 make -j
-echo "DEBUG smt_switch contents before"
-ls ./python/smt_switch
 # cibuildwheel doesn't want the generated .so file for the packaged Python
 # That happens automatically when built with python bindings
 # delete them now and let cibuildwheel do the packaging
 find ./python -name "*.so" -delete
 find ./python -name "*.o" -delete
-echo "DEBUG smt_switch contents after"
-ls ./python/smt_switch
