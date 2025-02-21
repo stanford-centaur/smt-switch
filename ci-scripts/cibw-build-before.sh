29bd69913860db7ae63d8e545dd5bf4519237956
Makai Mann <makaim@stanford.edu>
2025-02-20 23:55:20 -0500

    Make it clear that dependencies are only built once

--- ci-scripts/cibw-build-before.sh
+++ ci-scripts/cibw-build-before.sh
@@ -11,7 +11,8 @@
 #  directory for licensing information.\endverbatim
 ###############################################################################
 #
-# This script is meant to build the smt-switch C++ libraries for the PyPi upload
+# This script builds the smt-switch C++ libraries for the PyPi upload
+# It is meant to be run after cibw-build-deps.sh
 # cibuildwheel (abbreviated cibw) will take care of building and packaging the Python wheel
 
 set -e
@@ -21,12 +22,6 @@ set -e
 # a nonstandard location that CMake has trouble finding
 PYTHON_EXECUTABLE=$(which python3)
 echo "Using Python_EXECUTABLE: ${PYTHON_EXECUTABLE}"
-
-./contrib/setup-bitwuzla.sh
-# the version of ld.gold is too old in manylinux_2_28, remove it so cvc5 falls back on bfd
-rm -f /usr/bin/ld.gold
-./contrib/setup-cvc5.sh
-./contrib/setup-z3.sh
 
 ./configure.sh --bitwuzla --cvc5 --z3 --python --python-executable=${PYTHON_EXECUTABLE}
 cd build
