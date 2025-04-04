16b34ccd83ee9153439305897461da654a524727
Áron Ricardo Perez-Lopez <arpl@cs.stanford.edu>
2025-04-04 11:10:03 -0700

    Install cvc5 into deps/install in contrib/setup-cvc5.sh (#369)
    * Remove unnecessary permission change
    
    * Set install prefix
    
    * Remove manually added compilation flags
    
    * Fix message

--- contrib/setup-cvc5.sh
+++ contrib/setup-cvc5.sh
@@ -21,20 +21,22 @@ $DIR/setup-cadical.sh
 if [ ! -d "$DEPS/cvc5" ]; then
     cd $DEPS
     git clone https://github.com/cvc5/cvc5.git
-    chmod -R 777 cvc5
     cd cvc5
     git checkout -f ${CVC5_VERSION}
-    CXXFLAGS=-fPIC CFLAGS=-fPIC ./configure.sh --static --auto-download --dep-path="$DEPS/install"
+    ./configure.sh --prefix=$DEPS/install --static --auto-download --dep-path="$DEPS/install"
     cd build
     make -j$NUM_CORES
+    make install
     cd $DIR
 else
     echo "$DEPS/cvc5 already exists. If you want to rebuild, please remove it manually."
 fi
 
-if [ -f $DEPS/cvc5/build/src/libcvc5.a ] && [ -f $DEPS/cvc5/build/src/parser/libcvc5parser.a ] && [ -f $DEPS/install/lib/libcadical.a ]; then
+LIBS="$DEPS/install/lib"
+
+if [ -f $LIBS/libcvc5.a ] && [ -f $LIBS/libcvc5parser.a ] && [ -f $LIBS/libcadical.a ]; then
     echo "It appears cvc5 was setup successfully into $DEPS/cvc5."
-    echo "You may now install it with make ./configure.sh --cvc5 && cd build && make"
+    echo "You may now install it with ./configure.sh --cvc5 && cd build && make"
 else
     echo "Building cvc5 failed."
     echo "You might be missing some dependencies."
