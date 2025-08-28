e921a231b716ff35e35d15f781d9a0715960b6da
Áron Ricardo Perez-Lopez <arpl@cs.stanford.edu>
2025-08-28 10:39:41 -0700

    Allow extra options for `meson compile`

--- contrib/meson-steps.sh
+++ contrib/meson-steps.sh
@@ -1,16 +1,16 @@
 #!/bin/echo Script should be sourced from contrib/setup-xxx.sh, not executed directly:
-declare -a meson_options
+declare -a meson_setup_options meson_compile_options
 
 configure_step() {
-  meson_options+=(
+  meson_setup_options+=(
     -Dlibdir=lib  # makes sure libraries go into deps/install/lib
     -Dprefix="$install_dir"
   )
-  meson setup build "${meson_options[@]}"
+  meson setup build "${meson_setup_options[@]}"
 }
 
 build_step() {
-  meson compile -C build
+  meson compile -C build "${meson_compile_options[@]}"
 }
 
 install_step() {
