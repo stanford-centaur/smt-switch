8e6328c2aa1d2bc222124b669f61270ed35a40de
Áron Ricardo Perez-Lopez <arpl@cs.stanford.edu>
2025-10-10 20:11:27 -0700

    Update MathSAT to 5.6.12 and simplify script

--- ci-scripts/setup-msat.sh
+++ ci-scripts/setup-msat.sh
@@ -1,72 +1,62 @@
 #!/bin/bash
+set -euo pipefail
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-DEPS=$DIR/../deps
+script_dir="$(dirname "${BASH_SOURCE[0]}")"
+deps_dir="$(dirname "${script_dir}")"
 
-mkdir -p $DEPS
-
-
-usage () {
-    cat <<EOF
+usage() {
+  cat <<EOF
 Usage: $0 [<option> ...]
 
-Downloads the MathSAT SMT Solver. Note this solver is under a custom (non BSD compliant) license.
+Downloads the MathSAT SMT Solver. Note that this solver is under a custom (non BSD compliant) license.
 
 -h, --help              display this message and exit
 -y, --auto-yes          automatically agree to conditions (default: off)
 EOF
-    exit 0
+  exit 0
 }
 
 get_msat=default
-
-while [ $# -gt 0 ]
-do
-    case $1 in
-        -h|--help) usage;;
-        -y|--auto-yes) get_msat=y;;
-        *) die "unexpected argument: $1";;
-    esac
-    shift
+while (($# > 0)); do
+  case "$1" in
+    -h | --help) usage ;;
+    -y | --auto-yes) get_msat=y ;;
+    *) die "unexpected argument: $1" ;;
+  esac
+  shift
 done
 
-if [[ "$get_msat" == default ]]; then
-    read -p "MathSAT is distributed under a custom (non-BSD compliant) license. By continuing you acknowledge this distinction and assume responsibility for meeting the license conditions. Continue? [y]es/[n]o: " get_msat
+if [[ $get_msat == default ]]; then
+  read -rp "MathSAT is distributed under a custom (non-BSD compliant) license. By continuing you acknowledge this distinction and assume responsibility for meeting the license conditions. Continue? [y]es/[n]o: " get_msat
 fi
 
-if [[ "$get_msat" != y ]]; then
-    echo "Not downloading MathSAT"
-    exit 0
+if [[ $get_msat != y ]]; then
+  echo "Not downloading MathSAT"
+  exit 0
 fi
 
-RELEASE_URL="https://mathsat.fbk.eu/release/"
+msat_dir="${deps_dir}/mathsat"
+version="5.6.12"
+release_url="https://mathsat.fbk.eu/release"
 
-if [ ! -d "$DEPS/mathsat" ]; then
-    cd $DEPS
-    mkdir mathsat
-    if [[ "$OSTYPE" == linux* || "$OSTYPE" == cygwin* ]]; then
-        curl -o mathsat.tar.gz -L ${RELEASE_URL}mathsat-5.6.12-linux-x86_64.tar.gz
-    elif [[ "$OSTYPE" == darwin* ]]; then
-        curl -o mathsat.tar.gz -L ${RELEASE_URL}mathsat-5.6.12-macos.tar.gz
-    elif [[ "$OSTYPE" == msys* ]]; then
-        curl -o mathsat.tar.gz -L ${RELEASE_URL}mathsat-5.6.12-win64.zip
-    else
-        echo "Unrecognized OSTYPE=$OSTYPE"
-        exit 1
-    fi
-
-    tar -xf mathsat.tar.gz -C mathsat --strip-components 1
-    rm mathsat.tar.gz
-
+if [[ -d $msat_dir ]]; then
+  echo "${msat_dir} already exists. If you want to re-download, please remove it manually."
 else
-    echo "$DEPS/mathsat already exists. If you want to re-download, please remove it manually."
-fi
-
-if [ -f $DEPS/mathsat/lib/libmathsat.a ] ; then \
-    echo "It appears mathsat was setup successfully into $DEPS/mathsat."
-    echo "You may now install it with make ./configure.sh --msat && cd build && make"
-else
-    echo "Downloading mathsat failed."
-    echo "Please see their website: http://mathsat.fbk.eu/"
+  mkdir -p "${msat_dir}"
+  cd "${deps_dir}"
+  if [[ $OSTYPE =~ linux* ]]; then
+    wget -O mathsat.tar.gz "${release_url}/mathsat-${version}-linux-x86_64.tar.gz"
+  elif [[ $OSTYPE =~ darwin* ]]; then
+    wget -O mathsat.tar.gz "${release_url}/mathsat-${version}-macos-x86_64.tar.gz"
+  else
+    echo "Unrecognized OSTYPE ${OSTYPE}"
     exit 1
+  fi
+  tar -xf mathsat.tar.gz -C mathsat --strip-components 1
+  rm mathsat.tar.gz
+fi
+
+if [[ ! -f "${msat_dir}/lib/libmathsat.a" ]]; then
+  echo "Downloading mathsat failed."
+  exit 1
 fi
