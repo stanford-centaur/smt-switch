39ccb11da360321998140606c824ece1e86ff8ef
Áron Ricardo Perez-Lopez <arpl@cs.stanford.edu>
2025-11-12 09:43:54 -0800

    Add pkg-config file for Cadical (#435)
    This enables Bitwuzla to find it. And removes hacks with `C_INCLUDE_PATH` and `LIBRARY_PATH` to make builds reproducible.

--- contrib/setup-cadical.sh
+++ contrib/setup-cadical.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-git_tag=rel-2.1.3
+version_number=2.1.3
+git_tag=rel-$version_number
 github_owner=arminbiere
 
 configure_step() {
@@ -11,6 +12,10 @@ install_step() {
   install -m644 src/ccadical.h "$install_includedir"
   install -m644 src/cadical.hpp "$install_includedir"
   install -m644 src/tracer.hpp "$install_includedir"
+
+  export install_dir version_number
+  envsubst '$install_dir $version_number' <"$contrib_dir/pkgconfig/cadical.pc.in" >"$pkg_config_dir/cadical.pc"
+  export -n install_dir version_number
 }
 
 source "$(dirname "$0")/make-steps.sh"
