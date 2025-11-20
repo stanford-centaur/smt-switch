f8bf9129f167af0df5d2cd2c94d457b999055f9e
Áron Ricardo Perez-Lopez <arpl@cs.stanford.edu>
2025-11-20 13:18:41 -0800

    Add bitwuzla printing test (#466)
    * Add Bitwuzla printing test
    
    * Convert cvc5 printing test to gtest
    
    * Convert mathsat printing test to gtest

--- tests/msat/msat-printing.cpp
+++ tests/msat/msat-printing.cpp
@@ -13,15 +13,11 @@
 **
 **
 **/
-#include <array>
+#include <gtest/gtest.h>
+
 #include <cassert>
-#include <cstddef>
-#include <cstdio>
-#include <fstream>
 #include <iostream>
-#include <memory>
 #include <sstream>
-#include <stdexcept>
 #include <string>
 #include <unordered_set>
 #include <vector>
@@ -33,34 +29,29 @@
 
 using namespace smt;
 
-/**
- * A function for running a process
- * Taken from:
- * https://stackoverflow.com/questions/478898/how-do-i-execute-a-command-and-get-the-output-of-the-command-within-c-using-po
- */
-std::string exec(const char * cmd)
-{
-  std::array<char, 128> buffer;
-  std::string result;
-  std::unique_ptr<FILE> pipe(popen(cmd, "r"));
-  if (!pipe)
-  {
-    throw std::runtime_error("popen() failed!");
-  }
-  while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
-  {
-    result += buffer.data();
-  }
-  return result;
-}
+namespace smt_tests {
 
-int main()
+class MsatPrintingTest : public testing::Test
 {
+ protected:
+  void SetUp() override { os = new std::ostream(&strbuf); }
+  void TearDown() override { delete os; }
+  void check_result(
+      std::vector<std::unordered_set<std::string>> expected_result,
+      std::string extra_opts = "")
+  {
+    dump_and_run(MSAT, strbuf, expected_result, extra_opts);
+  }
   std::stringbuf strbuf;
-  SmtSolver msat = MsatSolverFactory::create_interpolating_solver();
-  std::ostream os(&strbuf);
+  std::ostream * os;
+};
+
+TEST_F(MsatPrintingTest, Interpolation)
+{
   SmtSolver s =
-      create_printing_solver(msat, &os, PrintingStyleEnum::MSAT_STYLE);
+      create_printing_solver(MsatSolverFactory::create_interpolating_solver(),
+                             os,
+                             PrintingStyleEnum::MSAT_STYLE);
   s->set_logic("QF_LIA");
 
   Sort intsort = s->make_sort(INT);
@@ -81,54 +72,14 @@ int main()
   Result r1 = s->get_interpolant(A1, B1, I1);
   assert(r1.is_unsat());
 
-  std::string filename = "msat-printing.cpp-sample.smt2";
-  std::ofstream out(filename.c_str());
-  out << strbuf.str() << std::endl;
-  out.close();
-  // MSAT_HOME is a macro defined when built with MSAT
-  // that points to the top-level MSAT directory
-  // STRFY is defined in test-utils.h and converts
-  // a macro to its string representation
-  std::string command(STRFY(MSAT_HOME));
-  command += "/bin/mathsat -interpolation=true ";
-  command += filename;
-  std::cout << "Running command: " << command << std::endl;
-  std::string result = exec(command.c_str());
-  std::cout << "got result:\n" << result << std::endl;
-
-  const std::string unsat_result = "unsat";
-  const std::vector<std::unordered_set<std::string>> interp_results = {
-    { "(<= 2 (+ z (* (- 1) x)))", "(<= (+ x (* (- 1) z)) (- 2))" },
-    { "(<= 2 (+ x (* (- 1) z)))", "(<= (+ z (* (- 1) x)) (- 2))" }
-  };
-  bool expect_interp = false;
-  std::size_t interp_index = 0;
-  for (std::string::size_type pos = 0;; pos = result.find("\n", pos) + 1)
-  {
-    if (interp_index == interp_results.size())
-    {
-      break;
-    }
-    else if (expect_interp)
-    {
-      bool interp_result_found = false;
-      for (const auto & interp_result : interp_results.at(interp_index++))
+  check_result(
       {
-        if (result.substr(pos, interp_result.length()) == interp_result)
-        {
-          interp_result_found = true;
-          break;
-        }
-      }
-      assert(interp_result_found);
-    }
-    else
-    {
-      assert(result.substr(pos, unsat_result.size()) == unsat_result);
-    }
-    expect_interp = !expect_interp;
-  }
-
-  remove(filename.c_str());
-  return 0;
+          { "unsat" },
+          { "(<= 2 (+ z (* (- 1) x)))", "(<= (+ x (* (- 1) z)) (- 2))" },
+          { "unsat" },
+          { "(<= 2 (+ x (* (- 1) z)))", "(<= (+ z (* (- 1) x)) (- 2))" },
+      },
+      "-interpolation=true");
 }
+
+}  // namespace smt_tests
