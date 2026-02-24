04b7b90a01152b12e4edd263eb0f19acbc90ae60
Po-Chun Chien <po-chun.chien@sosy.ifi.lmu.de>
2026-02-24 14:33:31 +0100

    Downlaod Bitwuzla from its main repository
    Interpolation feature is now publicly available

--- contrib/setup-bitwuzla.sh
+++ contrib/setup-bitwuzla.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
-git_commit=42fc6749a2df0f9ef5b9482927b2e745af810ba9
-
-download_step() {
-  git clone git@github.com:bitwuzla/bitwuzla-interpolants.git "$dep_name"
-  git -C "$dep_name" checkout "$git_commit"
-}
+git_commit=2e7204ac7344947d5f8ee1eb8b3812ec9981cedf
 
 prepare_step() {
   "$contrib_dir/setup-cadical.sh"
-  # Remove GMP subproject -- broken, has been removed on main
-  sed -i 14,28d src/meson.build
-  rm -f subprojects/gmp-6.3.0.wrap
-  rm -rf subprojects/packagefiles/gmp-6.3.0
 }
 
 # shellcheck source=contrib/meson-setup.sh
