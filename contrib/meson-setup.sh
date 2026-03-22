3408250f283a7eff276f90a465c3f32883d192f9
Áron Ricardo Perez-Lopez <arpl@cs.stanford.edu>
2026-03-22 00:19:19 -0400

    Fix bitwuzla build (#490)
    Pono's build failed: https://github.com/stanford-centaur/pono/actions/runs/23316069523.
    
    The CI runs on main succeeded due to a bug with caching solver builds. I will have to figure out how to fix that later.

--- contrib/meson-setup.sh
+++ contrib/meson-setup.sh
@@ -1,13 +1,15 @@
 # shellcheck shell=bash
 declare -a meson_setup_options meson_compile_options
 
-configure_step() {
-  meson_setup_options+=(
-    -Dlibdir=lib # makes sure libraries go into deps/install/lib
-    -Dprefix="$install_dir"
-  )
-  meson setup build "${meson_setup_options[@]}"
-}
+# Can be overridden for projects that provide a custom config script.
+if ! declare -F configure_step >/dev/null; then
+  configure_step() {
+    meson_setup_options+=(
+      -Dprefix="$install_dir"
+    )
+    meson setup build "${meson_setup_options[@]}"
+  }
+fi
 
 build_step() {
   meson compile -C build ${meson_compile_options[@]+"${meson_compile_options[@]}"}
--- contrib/setup-bitwuzla.sh
+++ contrib/setup-bitwuzla.sh
@@ -5,5 +5,9 @@ prepare_step() {
   "$contrib_dir/setup-cadical.sh"
 }
 
+configure_step() {
+  ./configure.py --prefix "$install_dir"
+}
+
 # shellcheck source=contrib/meson-setup.sh
 source "$(dirname "$(realpath "$0")")/meson-setup.sh"
