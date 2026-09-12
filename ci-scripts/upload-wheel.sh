6fe2887870f2e345ba0750a515a10219628a7100
Áron Ricardo Perez-Lopez <arpl@cs.stanford.edu>
2026-09-11 22:30:14 -0400

    fix: Change python back to python3 (#561)
    There is no binary called python on macOS.

--- ci-scripts/upload-wheel.sh
+++ ci-scripts/upload-wheel.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
-python -m venv .uploadenv
+python3 -m venv .uploadenv
 # shellcheck source=/dev/null  # activate script created by the command above
 source .uploadenv/bin/activate
 pip install twine
