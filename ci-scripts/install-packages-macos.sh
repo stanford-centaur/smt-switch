5b0fb9a5b8d9496294a452a3c79a36a1a3d01e39
Áron Ricardo Perez-Lopez <arpl@cs.stanford.edu>
2026-09-23 19:32:03 -0400

    ci: Do not set PATH manually on macOS
    Fragile and our builds should work without it, as users cannot be expected to know to do this.

--- ci-scripts/install-packages-macos.sh
+++ ci-scripts/install-packages-macos.sh
@@ -14,5 +14,4 @@ brew_prefix=$(brew --prefix)
   echo "CPATH=$brew_prefix/include"
   echo "LIBRARY_PATH=$brew_prefix/lib"
   echo "PKG_CONFIG_PATH=$brew_prefix/lib/pkgconfig"
-  echo "PATH=$brew_prefix/bin:$PATH"
 } >>"${GITHUB_ENV:?}"
