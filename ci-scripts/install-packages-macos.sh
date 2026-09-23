3518fa0b82cd69ef516fff35ba5dcd58a8f8db66
Áron Ricardo Perez-Lopez <arpl@cs.stanford.edu>
2026-09-23 17:28:05 -0400

    build: Use auto-provisioned bison on macOS (#618)
    This exercises the scenario where users don't have it installed locally
    and gets rid of a bunch of extra path manipulation as a bonus.

--- ci-scripts/install-packages-macos.sh
+++ ci-scripts/install-packages-macos.sh
@@ -4,17 +4,15 @@ set -euo pipefail
 brew update
 brew install \
   autoconf \
-  bison \
   gperf \
   meson \
   python-packaging
 
 brew_prefix=$(brew --prefix)
-bison_prefix=$(brew --prefix bison)
 
 {
   echo "CPATH=$brew_prefix/include"
   echo "LIBRARY_PATH=$brew_prefix/lib"
   echo "PKG_CONFIG_PATH=$brew_prefix/lib/pkgconfig"
-  echo "PATH=$bison_prefix/bin:$brew_prefix/bin:$PATH"
+  echo "PATH=$brew_prefix/bin:$PATH"
 } >>"${GITHUB_ENV:?}"
